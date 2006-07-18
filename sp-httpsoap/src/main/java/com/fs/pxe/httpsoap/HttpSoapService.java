4a505749e732f3f366114dd9a8c0403753676cf1
Alex Boisvert <boisvert@apache.org>
2006-07-18 18:38:23 +0000

    Cleanup imports
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ode/scratch/pxe-iapi@423183 13f79535-47bb-0310-9956-ffa450edef68

--- sp-httpsoap/src/main/java/com/fs/pxe/httpsoap/HttpSoapService.java
+++ sp-httpsoap/src/main/java/com/fs/pxe/httpsoap/HttpSoapService.java
@@ -35,7 +35,6 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 /**
  * A SOAP "adapter" service.
