9d8af5da7ce7ca791c98376732399a9ef333536d
Alex Boisvert <boisvert@apache.org>
2006-07-18 18:36:37 +0000

    Add JBI end-point-reference namespace
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/ode/scratch/pxe-iapi@423180 13f79535-47bb-0310-9956-ffa450edef68

--- utils/src/main/java/com/fs/utils/Namespaces.java
+++ utils/src/main/java/com/fs/utils/Namespaces.java
@@ -18,5 +18,7 @@ public class Namespaces {
   public static final String WSDL_20 = "http://www.w3.org/2006/01/wsdl";
 
   public static final String XML_SCHEMA = "http://www.w3.org/2001/XMLSchema";
+  
+  public static final String JBI_END_POINT_REFERENCE = "http://java.sun.com/jbi/end-point-reference";  
 
 }
