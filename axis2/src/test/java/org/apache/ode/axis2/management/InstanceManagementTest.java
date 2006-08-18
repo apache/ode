13b12d6781c1697fae8cb160760f4c39f0fbc68e
Matthieu Riou <mriou@apache.org>
2006-08-18 00:15:20 +0000

    ODE-27 ODE-14 More management API tests, more bug fixes, more pain. But almost done.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ode/trunk@432442 13f79535-47bb-0310-9956-ffa450edef68

--- axis2/src/test/java/org/apache/ode/axis2/management/InstanceManagementTest.java
+++ axis2/src/test/java/org/apache/ode/axis2/management/InstanceManagementTest.java
@@ -20,7 +20,210 @@
 package org.apache.ode.axis2.management;
 
 import junit.framework.TestCase;
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.OMNamespace;
+import org.apache.axiom.om.OMText;
+import org.apache.axiom.om.util.Base64;
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.addressing.EndpointReference;
+import org.apache.axis2.client.Options;
+import org.apache.axis2.client.ServiceClient;
+import org.apache.ode.tools.sendsoap.cline.HttpSoapSender;
+
+import javax.xml.namespace.QName;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class InstanceManagementTest extends TestCase {
+
+    public static final String PMAPI_NS = "http://www.apache.org/ode/pmapi/types/2006/08/02/";
+
+    private OMFactory _factory;
+    private DateFormat xsdDF = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+
+    public void testListInstances() throws Exception {
+        OMElement listRoot = buildMessage("listInstances", new String[] {"filter", "order", "limit"},
+                new String[] {"name=DynPartnerMain", "", "10"});
+        OMElement result = sendToIM(listRoot);
+        // Ensures that there's only 2 process-info string (ending and closing tags) and hence only one process
+        assert(result.toString().split("instance-info").length == 3);
+
+        // Another query with more options
+        Calendar notSoLongAgo = Calendar.getInstance();
+        notSoLongAgo.add(Calendar.MINUTE, -2);
+        String notSoLongAgoStr = xsdDF.format(notSoLongAgo.getTime());
+        listRoot = buildMessage("listInstances", new String[] {"filter", "order", "limit"},
+                new String[] {"name=DynPartnerResponder namespace=http://ode/bpel/responder " +
+                        "started>=" + notSoLongAgoStr, "", "10"});
+        result = sendToIM(listRoot);
+        assert(result.toString().split("instance-info").length == 5);
+    }
+
+    public void testListAllInstances() throws Exception {
+        OMElement root = buildMessage("listAllInstancesWithLimit", new String[] {"limit"}, new String[] {"1"});
+        OMElement result = sendToIM(root);
+        // We shold have only one instance (so 2 opening/closing elmts)
+        assert(result.toString().split("instance-info").length == 3);
+        // And one of our executed instances are there
+        assert(result.toString().indexOf("DynPartnerMain") >= 0 ||
+                result.toString().indexOf("DynPartnerResponder") >= 0);
+    }
+
+    public void testGetInstanceInfo() throws Exception {
+        OMElement root = buildMessage("listAllInstances", new String[] {}, new String[] {});
+        OMElement result = sendToIM(root);
+        String iid = result.getFirstChildWithName(new QName(PMAPI_NS, "instance-info"))
+                .getFirstChildWithName(new QName(PMAPI_NS, "iid")).getText();
+        root = buildMessage("getInstanceInfo", new String[] {"iid"}, new String[] {iid});
+        result = sendToIM(root);
+        assert(result.toString().split("instance-info").length == 3);
+    }
+
+    public void testGetScopeInfo() throws Exception {
+        OMElement root = buildMessage("listAllInstances", new String[] {}, new String[] {});
+        OMElement result = sendToIM(root);
+        String siid = result.getFirstChildWithName(new QName(PMAPI_NS, "instance-info"))
+                .getFirstChildWithName(new QName(PMAPI_NS, "root-scope"))
+                .getAttributeValue(new QName(null, "siid"));
+        root = buildMessage("getScopeInfoWithActivity", new String[] {"siid", "activityInfo"},
+                new String[] {siid, "true"});
+        result = sendToIM(root);
+        assert(result.toString().split("scope-info").length == 3);
+        assert(result.toString().indexOf("activity-info") >= 0);
+    }
+
+    public void testGetVariableInfo() throws Exception {
+        OMElement root = buildMessage("listInstances", new String[] {"filter", "order", "limit"},
+                new String[] {"name=DynPartnerMain", "", "10"});
+        OMElement result = sendToIM(root);
+        String siid = result.getFirstChildWithName(new QName(PMAPI_NS, "instance-info"))
+                .getFirstChildWithName(new QName(PMAPI_NS, "root-scope"))
+                .getAttributeValue(new QName(null, "siid"));
+        root = buildMessage("getVariableInfo", new String[] {"sid", "varName"}, new String[] {siid, "dummy"});
+        result = sendToIM(root);
+        assert(result.toString().indexOf("fire!") >= 0);
+    }
+
+    public void testListEvents() throws Exception {
+        OMElement root = buildMessage("listEvents", new String[] {"instanceFilter", "eventFilter", "maxCount"},
+                new String[] {"", "", "0"});
+        OMElement result = sendToIM(root);
+        assert(result.toString().split("event-info").length > 10);
+    }
+
+    public void testGetEventTimeline() throws Exception {
+        OMElement root = buildMessage("getEventTimeline", new String[] {"instanceFilter", "eventFilter"},
+                new String[] {"", ""});
+        OMElement result = sendToIM(root);
+        assert(result.toString().split("element").length > 10);
+    }
+
+    protected void setUp() throws Exception {
+        // Create a factory
+        _factory = OMAbstractFactory.getOMFactory();
+
+        // Use the factory to create three elements
+        OMNamespace depns = _factory.createOMNamespace(PMAPI_NS, "deployapi");
+        OMElement root = _factory.createOMElement("deploy", null);
+        OMElement namePart = _factory.createOMElement("name", depns);
+        namePart.setText("DynPartner");
+        OMElement zipPart = _factory.createOMElement("package", depns);
+        OMElement zipElmt = _factory.createOMElement("zip", depns);
+
+        // Add the zip to deploy
+        InputStream is = getClass().getClassLoader().getResourceAsStream("DynPartner.zip");
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        for (int b = is.read(); b >= 0; b = is.read()) {
+            outputStream.write((byte) b);
+        }
+        String base64Enc = Base64.encode(outputStream.toByteArray());
+        OMText zipContent = _factory.createOMText(base64Enc, "application/zip", true);
+        root.addChild(namePart);
+        root.addChild(zipPart);
+        zipPart.addChild(zipElmt);
+        zipElmt.addChild(zipContent);
+
+        // Deploy
+        sendToDeployment(root);
+
+        // Execute
+        URL svcUrl = new URL("http://localhost:8080/ode/processes/DynMainService");
+        InputStream sis = getClass().getClassLoader().getResourceAsStream("testDynPartnerRequest.soap");
+        HttpSoapSender.doSend(svcUrl, sis, System.out);
+        // Just making sure the instance starts
+        Thread.sleep(1000);
+    }
+
+    protected void tearDown() throws Exception {
+        // Prepare undeploy message
+        OMNamespace depns = _factory.createOMNamespace(PMAPI_NS, "deployapi");
+        OMElement root = _factory.createOMElement("undeploy", depns);
+        OMElement part = _factory.createOMElement("processName", null);
+        part.setText("DynPartner");
+        root.addChild(part);
+
+        // Undeploy
+        sendToDeployment(root);
+
+        OMElement listRoot = buildMessage("listProcesses", new String[] {"filter", "orderKeys"},
+                new String[] {"name=DynPartnerMain", ""});
+        OMElement result = sendToPM(listRoot);
+        assert(result.toString().indexOf("process-info") < 0);
+    }
+
+    private OMElement sendToPM(OMElement msg) throws AxisFault {
+        return send(msg, "http://localhost:8080/ode/services/ProcessManagement");
+    }
+
+    private OMElement sendToIM(OMElement msg) throws AxisFault {
+        return send(msg, "http://localhost:8080/ode/services/InstanceManagement");
+    }
+
+    private OMElement sendToDeployment(OMElement msg) throws AxisFault {
+        return send(msg, "http://localhost:8080/ode/services/DeploymentService");
+    }
+
+    private OMElement send(OMElement msg, String url) throws AxisFault {
+        Options options = new Options();
+        EndpointReference target = new EndpointReference(url);
+        options.setTo(target);
+
+        ServiceClient serviceClient = new ServiceClient();
+        serviceClient.setOptions(options);
+
+        return serviceClient.sendReceive(msg);
+    }
+
+    private OMElement buildMessage(String operation, String[] params, Object[] values) {
+        OMNamespace pmns = _factory.createOMNamespace(PMAPI_NS, "pmapi");
+        OMElement root = _factory.createOMElement(operation, pmns);
+        for (int m = 0; m < params.length; m++) {
+            OMElement omelmt = _factory.createOMElement(params[m], null);
+            if (values[m] instanceof String)
+                omelmt.setText((String) values[m]);
+            else if (values[m] instanceof QName)
+                omelmt.setText((QName) values[m]);
+            else if (values[m] instanceof OMElement)
+                omelmt.addChild((OMElement) values[m]);
+            else if (values[m] instanceof Object[]) {
+                Object[] subarr = (Object[]) values[m];
+                String elmtName = (String) subarr[0];
+                for (int p = 1; p < subarr.length; p++) {
+                    OMElement omarrelmt = _factory.createOMElement(elmtName, null);
+                    omarrelmt.setText(subarr[p].toString());
+                    omelmt.addChild(omarrelmt);
+                }
+            } else throw new UnsupportedOperationException("Type " + values[m].getClass() + "isn't supported as " +
+                    "a parameter type (only String and QName are).");
+            root.addChild(omelmt);
+        }
+        return root;
+    }
 
 }
--- tools/src/main/java/org/apache/ode/tools/sendsoap/cline/HttpSoapSender.java
+++ tools/src/main/java/org/apache/ode/tools/sendsoap/cline/HttpSoapSender.java
@@ -18,21 +18,32 @@
  */
 package org.apache.ode.tools.sendsoap.cline;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.apache.ode.tools.ToolMessages;
 import org.apache.ode.utils.StreamUtils;
-import org.apache.ode.utils.cli.*;
+import org.apache.ode.utils.cli.Argument;
+import org.apache.ode.utils.cli.BaseCommandlineTool;
+import org.apache.ode.utils.cli.CommandlineFragment;
+import org.apache.ode.utils.cli.ConsoleFormatter;
+import org.apache.ode.utils.cli.FlagWithArgument;
+import org.apache.ode.utils.cli.Fragments;
 import org.apache.ode.utils.msg.CommonMessages;
 import org.apache.ode.utils.msg.MessageBundle;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.StringRequestEntity;
 
 /**
  * <p>
@@ -41,109 +52,107 @@ import org.apache.commons.httpclient.methods.StringRequestEntity;
  */
 public class HttpSoapSender extends BaseCommandlineTool {
 
-  private static final int RETURN_BAD_URL = 2;
-  private static final int RETURN_SEND_ERROR = 3;
-  private static final int RETURN_CANT_READ = 4;
-  private static final int RETURN_CANT_WRITE = 5;
-  
-  private static Pattern SEQ = Pattern.compile("\\$sequence\\$");
+    private static final int RETURN_BAD_URL = 2;
+    private static final int RETURN_SEND_ERROR = 3;
+    private static final int RETURN_CANT_READ = 4;
+    private static final int RETURN_CANT_WRITE = 5;
 
-  private static final ToolMessages MESSAGES = MessageBundle.getMessages(ToolMessages.class);
-  private static final CommonMessages COMMON = MessageBundle.getMessages(CommonMessages.class);
-  
-  private static final Argument URL_A = new Argument("url","the URL to send the SOAP to.",false);
-  private static final Argument FILE_A = new Argument("file","the file that contains the SOAP to send.",false);
-  
-  private static final FlagWithArgument OUTFILE_FWA = new FlagWithArgument("o","outfile",
-      "a file to write the output to (instead of standard out).",true);
-    
-  private static final Fragments CLINE = new Fragments(new CommandlineFragment[] {
-     OUTFILE_FWA,URL_A, FILE_A 
-  });
-  
-  private static final String SYNOPSIS = 
-    "send the contents of a file to a URL as a SOAP request and print the response (if any) to the console or a file.";
-  
-  private static void doSend(URL u, InputStream is, OutputStream os)
-    throws IOException
-  {
-    ByteArrayOutputStream bos = new ByteArrayOutputStream(8192);
-    StreamUtils.copy(bos, is);
-    String now = Long.toString(System.currentTimeMillis());
-    int c = 1;
-    String data = new String(bos.toByteArray());
-    Matcher m = SEQ.matcher(data);
-    StringBuffer sb = new StringBuffer(8192);
-    while (m.find()) {
-        m.appendReplacement(sb, now + "-" + c++);
+    private static Pattern SEQ = Pattern.compile("\\$sequence\\$");
+
+    private static final ToolMessages MESSAGES = MessageBundle.getMessages(ToolMessages.class);
+    private static final CommonMessages COMMON = MessageBundle.getMessages(CommonMessages.class);
+
+    private static final Argument URL_A = new Argument("url","the URL to send the SOAP to.",false);
+    private static final Argument FILE_A = new Argument("file","the file that contains the SOAP to send.",false);
+
+    private static final FlagWithArgument OUTFILE_FWA = new FlagWithArgument("o","outfile",
+            "a file to write the output to (instead of standard out).",true);
+
+    private static final Fragments CLINE = new Fragments(new CommandlineFragment[] {
+            OUTFILE_FWA,URL_A, FILE_A
+    });
+
+    private static final String SYNOPSIS =
+            "send the contents of a file to a URL as a SOAP request and print the response (if any) to the console or a file.";
+
+    public static void doSend(URL u, InputStream is, OutputStream os) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(8192);
+        StreamUtils.copy(bos, is);
+        String now = Long.toString(System.currentTimeMillis());
+        int c = 1;
+        String data = new String(bos.toByteArray());
+        Matcher m = SEQ.matcher(data);
+        StringBuffer sb = new StringBuffer(8192);
+        while (m.find()) {
+            m.appendReplacement(sb, now + "-" + c++);
+        }
+        m.appendTail(sb);
+        HttpClient httpClient = new HttpClient();
+        PostMethod httpPostMethod = new PostMethod(u.toExternalForm());
+        // TODO what about this header?
+        //_httpPostMethod.setRequestHeader("SOAPAction", writer.getSoapAction());
+        httpPostMethod.setRequestHeader("Content-Type", "text/xml");
+        httpPostMethod.setRequestEntity(new StringRequestEntity(sb.toString()));
+        httpClient.executeMethod(httpPostMethod);
+        String response = httpPostMethod.getResponseBodyAsString();
+        if (response != null) {
+            os.write(response.getBytes());
+            os.write("\n".getBytes());
+        }
     }
-    m.appendTail(sb);
-    HttpClient httpClient = new HttpClient();
-    PostMethod httpPostMethod = new PostMethod(u.toExternalForm());
-    // TODO what about this header?
-    //_httpPostMethod.setRequestHeader("SOAPAction", writer.getSoapAction());
-    httpPostMethod.setRequestHeader("Content-Type", "text/xml");
-    httpPostMethod.setRequestEntity(new StringRequestEntity(sb.toString()));
-    httpClient.executeMethod(httpPostMethod);
-    String response = httpPostMethod.getResponseBodyAsString();
-    if (response != null) {
-    	os.write(response.getBytes());
-      os.write("\n".getBytes());
+
+    public static void main(String[] argv) {
+        if (argv.length == 0 || HELP.matches(argv)) {
+            ConsoleFormatter.printSynopsis(getProgramName(),SYNOPSIS,new Fragments[] {
+                    CLINE,HELP});
+            System.exit(0);
+        } else if (!CLINE.matches(argv)) {
+            consoleErr("INVALID COMMANDLINE: Try \"" + getProgramName() + " -h\" for help.");
+            System.exit(-1);
+        }
+        OutputStream os = null;
+        if (OUTFILE_FWA.isSet()) {
+            String outfile = OUTFILE_FWA.getValue();
+            File f = new File(outfile);
+            try {
+                os = new FileOutputStream(f);
+            } catch (FileNotFoundException fnfe) {
+                consoleErr(COMMON.msgCannotWriteToFile(outfile));
+                System.exit(RETURN_CANT_WRITE);
+            }
+        } else {
+            os = System.out;
+        }
+
+        URL u = null;
+        try {
+            u = new URL(URL_A.getValue());
+        } catch (MalformedURLException mue) {
+            consoleErr(MESSAGES.msgBadUrl(URL_A.getValue(),mue.getMessage()));
+            System.exit(RETURN_BAD_URL);
+        }
+
+        InputStream is = null;
+
+        String src = FILE_A.getValue();
+        if (src.equals("-")) {
+            is = System.in;
+        } else {
+            File f = new File(src);
+            try {
+                is = new FileInputStream(f);
+            } catch (FileNotFoundException fnfe) {
+                consoleErr(COMMON.msgCannotReadFromFile(src));
+                System.exit(RETURN_CANT_READ);
+            }
+        }
+
+        initLogging();
+        try{
+            doSend(u,is,os);
+        } catch (IOException ioe) {
+            consoleErr(MESSAGES.msgIoErrorOnSend(ioe.getMessage()));
+            System.exit(RETURN_SEND_ERROR);
+        }
     }
-  }
-  
-  public static void main(String[] argv) {
-    if (argv.length == 0 || HELP.matches(argv)) {
-      ConsoleFormatter.printSynopsis(getProgramName(),SYNOPSIS,new Fragments[] {
-        CLINE,HELP});
-      System.exit(0);
-    } else if (!CLINE.matches(argv)) {
-      consoleErr("INVALID COMMANDLINE: Try \"" + getProgramName() + " -h\" for help.");
-      System.exit(-1);
-    }
-    OutputStream os = null;
-    if (OUTFILE_FWA.isSet()) {
-      String outfile = OUTFILE_FWA.getValue();
-      File f = new File(outfile);
-      try {
-        os = new FileOutputStream(f);
-      } catch (FileNotFoundException fnfe) {
-        consoleErr(COMMON.msgCannotWriteToFile(outfile));
-        System.exit(RETURN_CANT_WRITE);
-      }
-    } else {
-      os = System.out;
-    }
-            
-    URL u = null;
-    try {
-      u = new URL(URL_A.getValue());
-    } catch (MalformedURLException mue) {
-      consoleErr(MESSAGES.msgBadUrl(URL_A.getValue(),mue.getMessage()));
-      System.exit(RETURN_BAD_URL);
-    }
-    
-    InputStream is = null;
-    
-    String src = FILE_A.getValue();
-    if (src.equals("-")) {
-      is = System.in;
-    } else {
-      File f = new File(src);
-      try {
-        is = new FileInputStream(f);
-      } catch (FileNotFoundException fnfe) {
-        consoleErr(COMMON.msgCannotReadFromFile(src));
-        System.exit(RETURN_CANT_READ);
-      }
-    }
-    
-    initLogging();
-    try{
-      doSend(u,is,os);
-    } catch (IOException ioe) {
-      consoleErr(MESSAGES.msgIoErrorOnSend(ioe.getMessage()));
-      System.exit(RETURN_SEND_ERROR);
-    }
-  }
 }
