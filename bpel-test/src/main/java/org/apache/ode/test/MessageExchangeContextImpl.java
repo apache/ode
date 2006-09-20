5cc9f490a1b20f0484a3f583c4942c3ecde75c40
Matthieu Riou <mriou@apache.org>
2006-09-20 15:01:33 +0000

    Applying Apache license headers.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/ode/trunk@448220 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-test/src/main/java/org/apache/ode/test/MessageExchangeContextImpl.java
+++ bpel-test/src/main/java/org/apache/ode/test/MessageExchangeContextImpl.java
@@ -17,10 +17,26 @@
  * under the License.
  */
 
-package org.apache.ode.test;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
-import javax.wsdl.PortType;
-import javax.xml.namespace.QName;
+package org.apache.ode.test;
 
 import org.apache.ode.bpel.iapi.BpelEngineException;
 import org.apache.ode.bpel.iapi.ContextException;
@@ -29,6 +45,8 @@ import org.apache.ode.bpel.iapi.MessageExchangeContext;
 import org.apache.ode.bpel.iapi.MyRoleMessageExchange;
 import org.apache.ode.bpel.iapi.PartnerRoleMessageExchange;
 import org.w3c.dom.Element;
+
+import javax.xml.namespace.QName;
 
 /**
  * This is a simple MessageExchangeContext implementation
