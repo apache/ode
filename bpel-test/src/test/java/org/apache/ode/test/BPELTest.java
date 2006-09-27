31d66eae3875e06e334d7e1621268303f85d5d78
Lance Waterman <lwaterman@apache.org>
2006-09-27 14:45:08 +0000

    Get multi message tests working
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ode/trunk@450464 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-test/src/test/java/org/apache/ode/test/BPELTest.java
+++ bpel-test/src/test/java/org/apache/ode/test/BPELTest.java
@@ -22,6 +22,7 @@ import junit.framework.TestCase;
 import org.apache.ode.bpel.engine.BpelServerImpl;
 import org.apache.ode.bpel.iapi.Message;
 import org.apache.ode.bpel.iapi.MyRoleMessageExchange;
+import org.apache.ode.bpel.iapi.MessageExchange.Status;
 import org.apache.ode.bpel.memdao.BpelDAOConnectionFactoryImpl;
 import org.apache.ode.test.scheduler.TestScheduler;
 import org.apache.ode.utils.DOMUtils;
@@ -35,14 +36,16 @@ import java.util.regex.Pattern;
 public class BPELTest extends TestCase {
 	
 	private BpelServerImpl server;
+	private MessageExchangeContextImpl mexContext; 
 
 	@Override
 	protected void setUp() throws Exception {
 		server = new BpelServerImpl();
+		mexContext = new MessageExchangeContextImpl();
 		server.setDaoConnectionFactory(new BpelDAOConnectionFactoryImpl());
 		server.setScheduler(new TestScheduler());
 		server.setBindingContext(new BindingContextImpl());
-		server.setMessageExchangeContext(new MessageExchangeContextImpl());
+		server.setMessageExchangeContext(mexContext);
 		server.setDeploymentManager(new DeploymentManagerImpl());
 		server.init();
 		server.start();
@@ -55,40 +58,99 @@ public class BPELTest extends TestCase {
 	
 	private void go(String deployDir) throws Exception {
 		
-		File testPropsFile = new File(deployDir+"/test.properties");
-		if ( !testPropsFile.exists()) {
-			System.err.println("can't find "+ testPropsFile.toString());
-		}
-		Properties testProps = new Properties();
-		testProps.load(testPropsFile.toURL().openStream());
+		/**
+		 * The deploy directory must contain at least one
+		 * "test.properties" file. 
+		 * 
+		 * The test.properties file identifies the service, 
+		 * operation and messages to be sent to the BPEL engine.
+		 * 
+		 * The deploy directory may contain more than
+		 * one file in the form of "testN.properties" where N
+		 * represents a monotonic integer beginning with 1.
+		 *   
+		 */
 		
-		QName serviceId = new QName(testProps.getProperty("namespace"),
-				testProps.getProperty("service"));
-		String operation = testProps.getProperty("operation");
-
+		int propsFileCnt = 0;
+		File testPropsFile = new File(deployDir+"/test.properties");
+		
+		if ( !testPropsFile.exists()) {
+			propsFileCnt++;
+			testPropsFile = new File(deployDir+"/test"+propsFileCnt+".properties");
+			if ( !testPropsFile.exists()) {
+				System.err.println("can't find "+ testPropsFile.toString());
+			}
+		}
+		
 		server.getDeploymentService().deploy(new File(deployDir));
+		
+		while ( testPropsFile.exists() ) {
+		
+			Properties testProps = new Properties();
+			testProps.load(testPropsFile.toURL().openStream());
+			
+			QName serviceId = new QName(testProps.getProperty("namespace"),
+					testProps.getProperty("service"));
+			String operation = testProps.getProperty("operation");
+	
+			MyRoleMessageExchange mex = server.getEngine().createMessageExchange("",serviceId,operation);
+	
+	
+			/**
+			 * Each property file must contain at least one 
+			 * request/response property tuple.
+			 * 
+			 * The request/response tuple should be in the form
+			 * 
+			 * requestN=<message>some XML input message</message>
+			 * responseN=.*some response message.*
+			 * 
+			 * Where N is a monotonic integer beginning with 1.
+			 * 
+			 * If no response is expected use:
+			 * responseN=null
+			 * 
+			 */
+			
+			for (int i=1; testProps.getProperty("request"+i) != null; i++) {
+	
+				String in = testProps.getProperty("request"+i);
+				String responsePattern = testProps.getProperty("response"+i);
+	
+				mexContext.clearCurrentResponse();
+				
+				Message request = mex.createMessage(null);
+	
+				Element elem = DOMUtils.stringToDOM(in);
+				request.setMessage(elem);
+	
+				mex.invoke(request);
+				
+				switch (mex.getStatus()) {
+				case RESPONSE:
+					Message response = mex.getResponse();
+					String resp = DOMUtils.domToString(response.getMessage());
+					System.out.println(resp);
+					assertTrue(Pattern.compile(responsePattern,Pattern.DOTALL).matcher(resp).matches());
+					// TODO: test for response fault
+					break;
+				case ASYNC:
+					// TODO: handle Async
+					if ( !responsePattern.equals("null")) 
+						assertTrue(false);
+					break;
+				case FAULT:
+					// TODO: handle Fault
+					assertTrue(false);
+					break;
+				default:
+					assertTrue(false);
+					break;
+				} 
 
-		MyRoleMessageExchange mex = server.getEngine().createMessageExchange("",serviceId,operation);
-
-
-		for (int i=1; testProps.getProperty("request"+i) != null; i++) {
-
-			String in = testProps.getProperty("request"+i);
-			String responsePattern = testProps.getProperty("response"+i);
-
-			Message request = mex.createMessage(null);
-
-			Element elem = DOMUtils.stringToDOM(in);
-			String tmp = elem.toString();
-			request.setMessage(elem);
-
-			mex.invoke(request);
-
-			Message response = mex.getResponse();
-
-			String resp = DOMUtils.domToString(response.getMessage());
-			System.out.println(resp);
-			assertTrue(Pattern.compile(responsePattern,Pattern.DOTALL).matcher(resp).matches());
+			}
+			propsFileCnt++;
+			testPropsFile = new File(deployDir+"/test"+propsFileCnt+".properties");
 		}
 	}
 
@@ -112,6 +174,12 @@ public class BPELTest extends TestCase {
     public void testAssignActivity2() throws Exception {
         go("target/test-classes/bpel/2.0/TestAssignActivity2");
     }
+    public void testInstPick() throws Exception {
+        go("target/test-classes/bpel/2.0/TestInstantiatingPick");
+    }
+	public void testCorrelation() throws Exception {
+		go("target/test-classes/bpel/2.0/testCorrelation");
+	}
     
     /** These tests compile however they fail at runtime */
   
@@ -121,8 +189,6 @@ public class BPELTest extends TestCase {
 //	public void testTimer() throws Exception {
 //		go("target/test-classes/bpel/2.0/TestTimer");
 //	} 
-//	public void testCorrelation() throws Exception {
-//		go("target/test-classes/bpel/2.0/testCorrelation");
-//	}
+
 
 }
