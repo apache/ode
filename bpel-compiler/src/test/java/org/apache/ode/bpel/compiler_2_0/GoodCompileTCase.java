929e1527d71fd17b0b0f0e7d5b6f79c49d88af0b
Assaf Arkin <assaf@apache.org>
2006-10-06 22:03:35 +0000

    Running test cases by compiling 2.0 processes from bpel-scripts
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ode/trunk@453788 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-compiler/src/test/java/org/apache/ode/bpel/compiler_2_0/GoodCompileTCase.java
+++ bpel-compiler/src/test/java/org/apache/ode/bpel/compiler_2_0/GoodCompileTCase.java
@@ -37,35 +37,35 @@ import java.util.ArrayList;
  */
 class GoodCompileTCase extends TestCase implements CompileListener {
 
-  private BpelC _compiler;
-  private ArrayList<CompilationMessage> _errors = new ArrayList<CompilationMessage>();
-  private URL _bpelURL;
+    private BpelC _compiler;
+    private ArrayList<CompilationMessage> _errors = new ArrayList<CompilationMessage>();
+    private File  _bpel;
 
-  GoodCompileTCase(String bpel) {
-    super(bpel);
-    _bpelURL = getClass().getResource(bpel);
-  }
-
-  protected void setUp() throws Exception {
-    super.setUp();
-    _compiler = BpelC.newBpelCompiler();
-    _compiler.setCompileListener(this);
-    _compiler.setOutputStream(new ByteArrayOutputStream(StreamUtils.DEFAULT_BUFFER_SIZE));
-    _errors.clear();
-  }
-
-  public void runTest() throws Exception {
-    try {
-      _compiler.compile(new File(_bpelURL.toURI()));
-    } catch (Exception ex) {
-      ex.printStackTrace();
-      fail("Compilation did not succeed.");
+    GoodCompileTCase(String bpel) {
+        super(bpel);
+        _bpel = new File("../bpel-scripts/src/main/resources", bpel);
     }
-  }
 
-  public void onCompilationMessage(CompilationMessage compilationMessage) {
-    _errors.add(compilationMessage);
-    System.err.println(compilationMessage.toString());
-  }
+    protected void setUp() throws Exception {
+        super.setUp();
+        _compiler = BpelC.newBpelCompiler();
+        _compiler.setCompileListener(this);
+        _compiler.setOutputStream(new ByteArrayOutputStream(StreamUtils.DEFAULT_BUFFER_SIZE));
+        _errors.clear();
+    }
+
+    public void runTest() throws Exception {
+        try {
+            _compiler.compile(_bpel);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail("Compilation did not succeed.");
+        }
+    }
+
+    public void onCompilationMessage(CompilationMessage compilationMessage) {
+        _errors.add(compilationMessage);
+        System.err.println(compilationMessage.toString());
+    }
 
 }
