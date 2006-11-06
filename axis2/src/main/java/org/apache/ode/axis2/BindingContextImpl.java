361c8fabbf334f9eb2bcf2ad07f93a5b413ea20d
Alex Boisvert <boisvert@apache.org>
2006-11-06 23:42:54 +0000

    Cosmetics
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/ode/trunk@471929 13f79535-47bb-0310-9956-ffa450edef68

--- axis2/src/main/java/org/apache/ode/axis2/BindingContextImpl.java
+++ axis2/src/main/java/org/apache/ode/axis2/BindingContextImpl.java
@@ -17,25 +17,6 @@
  * under the License.
  */
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package org.apache.ode.axis2;
 
 import org.apache.axis2.AxisFault;
@@ -60,11 +41,11 @@ public class BindingContextImpl implements BindingContext {
         _store = store;
     }
 
-    public EndpointReference activateMyRoleEndpoint(QName processId, Endpoint myRoleEndpoint,
-            PortType portType) {
+    public EndpointReference activateMyRoleEndpoint(QName processId, Endpoint myRoleEndpoint, PortType portType) {
         try {
-            ODEService svc = _server.createService(_store.getDefinitionForService(processId, myRoleEndpoint.serviceName)
-                    , myRoleEndpoint.serviceName, myRoleEndpoint.portName);
+            ODEService svc = _server.createService(
+                _store.getDefinitionForService(processId, myRoleEndpoint.serviceName),
+                myRoleEndpoint.serviceName, myRoleEndpoint.portName);
             return svc.getMyServiceRef();
         } catch (AxisFault axisFault) {
             throw new ContextException("Could not activate endpoint for service " + myRoleEndpoint.serviceName
