48978375541454e02ff2adc89739b6f1ef61dc02
Lance Waterman <lwaterman@apache.org>
2006-11-21 06:42:23 +0000

    Extend unit test
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/ode/trunk@477529 13f79535-47bb-0310-9956-ffa450edef68

--- dao-jpa-ojpa-derby/src/test/java/org/apache/ode/dao/jpa/test/InsertObjectTest.java
+++ dao-jpa-ojpa-derby/src/test/java/org/apache/ode/dao/jpa/test/InsertObjectTest.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.apache.ode.bpel.dao.BpelDAOConnection;
+import org.apache.ode.bpel.dao.MessageExchangeDAO;
 import org.apache.ode.dao.jpa.ojpa.*;
 import org.apache.openjpa.persistence.ArgumentException;
 
@@ -20,38 +21,23 @@ public class InsertObjectTest extends TestCase {
 	protected void setUp() throws Exception {
 		
 		try {
-			
-		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("ode-unit-test");
-		em = emf.createEntityManager();
-		
-		if ( em.isOpen() ) {
-			System.out.println("connection open");
-		}
-		
-
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("ode-unit-test");
+			em = emf.createEntityManager();
 		
 		} catch ( Exception e ) {
 			e.printStackTrace();
+			fail();
 		}
 		
 		
 	}
 	
 	public void testStart() throws Exception {
-		System.out.println("start test");
-		String tmp = new String("some data");
 		
 		BPELDAOConnectionFactoryImpl factory = new BPELDAOConnectionFactoryImpl();
 		BpelDAOConnection conn = factory.getConnection();
 		
 		
-//	    Collection<MessageTest> tmp2 = ft.getFaultMessages();
-//		ft.addMessage("Test Message");
-//		ft.addProp("test1", "test prop 1");
-	    
-
-		//Collection<TestMessage> tmp = ft.g
 		try {
 		em.getTransaction().begin();
 		em.persist(conn);
@@ -60,6 +46,7 @@ public class InsertObjectTest extends TestCase {
 		em.getTransaction().commit();
 		} catch (Exception e) {
 			e.printStackTrace();
+			fail();
 		}
 		
 		
@@ -68,6 +55,10 @@ public class InsertObjectTest extends TestCase {
 	@Override
 	protected void tearDown() throws Exception {
 		em.close();
+	}
+	
+	private void createMessageExchange(BpelDAOConnection conn ) {
+		MessageExchangeDAO me = conn.createMessageExchange('0');
 	}
 	
 
