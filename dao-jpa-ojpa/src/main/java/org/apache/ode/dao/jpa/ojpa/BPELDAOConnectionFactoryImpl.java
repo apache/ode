c6db306f0b07bf57183a89b00997856154babe59
Lance Waterman <lwaterman@apache.org>
2006-11-28 08:36:23 +0000

    Add connection control
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/ode/trunk@479945 13f79535-47bb-0310-9956-ffa450edef68

--- dao-jpa-ojpa/src/main/java/org/apache/ode/dao/jpa/ojpa/BPELDAOConnectionFactoryImpl.java
+++ dao-jpa-ojpa/src/main/java/org/apache/ode/dao/jpa/ojpa/BPELDAOConnectionFactoryImpl.java
@@ -1,7 +1,11 @@
 package org.apache.ode.dao.jpa.ojpa;
 
 
+import java.util.List;
 import java.util.Properties;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import org.apache.ode.bpel.dao.BpelDAOConnection;
 import org.apache.ode.bpel.dao.BpelDAOConnectionFactory;
@@ -9,8 +13,42 @@ import org.apache.ode.dao.jpa.BPELDAOConnectionImpl;
 
 public class BPELDAOConnectionFactoryImpl implements BpelDAOConnectionFactory {
 
+	private EntityManager em;
+	
+	public BPELDAOConnectionFactoryImpl(EntityManager em) {
+		this.em = em;
+	}
+	
 	public BpelDAOConnection getConnection() {
-		return new BPELDAOConnectionImpl();
+		
+		Query q = em.createQuery("SELECT x FROM BPELDAOConnectionImpl x order by x._id asc");
+		
+		List<BpelDAOConnection> conns = (List<BpelDAOConnection>)q.getResultList();
+		
+		if ( conns.size() < 1 ) {
+			return new BPELDAOConnectionImpl(new Long(1));
+		}
+		
+		return conns.get(conns.size()-1);
+	}
+	
+	public BpelDAOConnection getConnection(Long connID) {
+		
+		Query q = em.createQuery("SELECT x FROM BPELDAOConnectionImpl x WHERE x._id = ?1");
+		q.setParameter(1, connID);
+		
+		BpelDAOConnection conn = (BpelDAOConnection)q.getSingleResult();
+		
+		if ( conn == null ) {
+			conn = new BPELDAOConnectionImpl(connID);
+		}
+		
+		return conn;
+	}
+	
+	public Long getConnectionId(BpelDAOConnection conn) {
+		BPELDAOConnectionImpl oConn = (BPELDAOConnectionImpl)conn;
+		return oConn.getID();
 	}
 
 	public void init(Properties properties) {
