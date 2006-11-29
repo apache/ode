2ab74d769eaffac4660d3ebbce9603f0a246d642
Maciej Szefler <mszefler@apache.org>
2006-11-29 16:48:47 +0000

    Added JACOB annotation processor to repository.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/ode/trunk@480619 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jacob-ap/src/main/java/org/apache/ode/jacob/ap/ChannelType.java
@@ -0,0 +1,9 @@
+package org.apache.ode.jacob.ap;
+
+/**
+ * Annotation used to indicate that an interface is a JACOB channel interface.
+ * @author Maciej Szefler <mszefler at gmail dot com>
+ */
+public @interface ChannelType {
+
+}
