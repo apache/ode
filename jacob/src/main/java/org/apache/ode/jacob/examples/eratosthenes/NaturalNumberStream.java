f89e6f1ebc0dd7d9e10977539d9a4b703e47de5b
Maciej Szefler <mszefler@apache.org>
2006-11-29 17:19:29 +0000

    JACOB annotation processor. Replaced XDOCLET Channel/ChannelListener generation with a JDK5.0 "apt" based version that uses annotations. The big benefit here is that it is now a lot easier to modify the code-generation since it is done in Java and not some crazy xdoclet language. Also, the code-generation is a lot faster and is a lot more flexible.
    Be aware of "compiler errors" in the jacob and bpel-runtime that aren't. This is an artifact of the 
    apt processing mechanism's interaction with our project structure. In any case, if you see what 
    look like compile errors, but then the build succeeds, don't worry.
    
    
    
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/ode/trunk@480631 13f79535-47bb-0310-9956-ffa450edef68

--- jacob/src/main/java/org/apache/ode/jacob/examples/eratosthenes/NaturalNumberStream.java
+++ jacob/src/main/java/org/apache/ode/jacob/examples/eratosthenes/NaturalNumberStream.java
@@ -19,6 +19,7 @@
 package org.apache.ode.jacob.examples.eratosthenes;
 
 import org.apache.ode.jacob.SynchChannel;
+import org.apache.ode.jacob.ap.ChannelType;
 
 /**
  * DOCUMENTME.
@@ -27,6 +28,7 @@ import org.apache.ode.jacob.SynchChannel;
  * @jacob.kind
  * @author Maciej Szefler <a href="mailto:mbs@fivesight.com">mbs</a>
  */
+@ChannelType
 public interface NaturalNumberStream {
   public void val(int n, SynchChannel ret);
 }
