f48d06219442db266aa66c0aa8fcb83e55f13025
Lance Waterman <lwaterman@apache.org>
2006-11-30 07:24:18 +0000

    Enhance unit test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ode/trunk@480865 13f79535-47bb-0310-9956-ffa450edef68

--- dao-jpa-ojpa-derby/src/test/java/org/apache/ode/dao/jpa/test/SelectObjectTest.java
+++ dao-jpa-ojpa-derby/src/test/java/org/apache/ode/dao/jpa/test/SelectObjectTest.java
@@ -1,11 +1,17 @@
 package org.apache.ode.dao.jpa.test;
 
+import java.util.Collection;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.xml.namespace.QName;
 
+import org.apache.ode.bpel.common.CorrelationKey;
 import org.apache.ode.bpel.dao.BpelDAOConnection;
+import org.apache.ode.bpel.dao.ProcessDAO;
+import org.apache.ode.bpel.dao.ProcessInstanceDAO;
 import org.apache.ode.dao.jpa.ojpa.BPELDAOConnectionFactoryImpl;
 
 import junit.framework.TestCase;
@@ -13,6 +19,10 @@ import junit.framework.TestCase;
 public class SelectObjectTest extends TestCase {
 	
 	private EntityManager em;
+	private static final String TEST_NS = "http://org.apache.ode.jpa.test";
+	private String[] correlationKeys = { "key1", "key2" };
+	private CorrelationKey key1 = new CorrelationKey(1,correlationKeys);
+
 
 	@Override
 	protected void setUp() throws Exception {
@@ -32,15 +42,22 @@ public class SelectObjectTest extends TestCase {
 	public void testGetObject() throws Exception {
 		
 		BPELDAOConnectionFactoryImpl factory = new BPELDAOConnectionFactoryImpl(em);
-		BpelDAOConnection conn1 = factory.getConnection();
-		Long connId1 = factory.getConnectionId(conn1);
-		BpelDAOConnection conn2 = factory.getConnection(connId1 + 5);
-		Long connId2 = factory.getConnectionId(conn2);
+		BpelDAOConnection conn = factory.getConnection();
 		
-		if ( conn1 != null && conn2 != null	 ) {
-			System.out.println("got conn1:" + connId1);
-			System.out.println("got conn2:" + connId2);
+		ProcessDAO p = conn.getProcess(new QName(TEST_NS,"testPID1"));
+		Collection<ProcessInstanceDAO> insts = p.findInstance(key1);
+		
+		for ( ProcessInstanceDAO inst : insts ) {
+			Long id = inst.getInstanceId();
 		}
+		
+		int instCount = p.getNumInstances();
+		
+		
+		
+		conn.close();
+		
+
 	}
 
 	@Override
