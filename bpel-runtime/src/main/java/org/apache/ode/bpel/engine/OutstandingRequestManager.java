81d7c13e2eaceb4dd942751d2922f46360783224
Assaf Arkin <assaf@apache.org>
2006-12-11 23:57:27 +0000

    Fix ClassCastException
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ode/trunk@485955 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-runtime/src/main/java/org/apache/ode/bpel/engine/OutstandingRequestManager.java
+++ bpel-runtime/src/main/java/org/apache/ode/bpel/engine/OutstandingRequestManager.java
@@ -200,7 +200,7 @@ class OutstandingRequestManager implements Serializable {
     }
     _byChannel.values().clear();
     _byRid.values().clear();
-    return (String[]) mexRefs.toArray(new String[]);
+    return (String[]) mexRefs.toArray(new String[0]);
   }
 
   public String toString() {
