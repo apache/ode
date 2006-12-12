d72ffc032ab9d779f305ecb96ad5aa85d6485035
Lance Waterman <lwaterman@apache.org>
2006-12-12 06:59:55 +0000

    Fixed several unique key problems
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ode/trunk@486048 13f79535-47bb-0310-9956-ffa450edef68

--- dao-jpa/src/main/java/org/apache/ode/dao/jpa/ProcessInstanceDAOImpl.java
+++ dao-jpa/src/main/java/org/apache/ode/dao/jpa/ProcessInstanceDAOImpl.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.ode.dao.jpa;
 
 import java.util.ArrayList;
@@ -96,6 +115,9 @@ public class ProcessInstanceDAOImpl implements ProcessInstanceDAO {
 		_scopes.add(ret);
 		
 		_rootScope = (parentScope == null)?ret:_rootScope;
+		
+		// Must persist the scope to generate a scope ID
+		_connection.getEntityManager().persist(ret);
 		
 		return ret;
 	}
@@ -262,6 +284,7 @@ public class ProcessInstanceDAOImpl implements ProcessInstanceDAO {
 	public void setFault(QName faultName, String explanation, int faultLineNo,
 			int activityId, Element faultMessage) {
 		_fault = new FaultDAOImpl(faultName,explanation,faultLineNo,activityId,faultMessage);
+		
 	}
 
 	public void setLastActiveTime(Date dt) {
