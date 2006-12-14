0a49107afabd853594f5f06904ad178db673296a
Lance Waterman <lwaterman@apache.org>
2006-12-14 07:18:44 +0000

    Add support for JPA
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ode/trunk@487052 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-test/src/test/java/org/apache/ode/test/DataHandling20Tests.java
+++ bpel-test/src/test/java/org/apache/ode/test/DataHandling20Tests.java
@@ -2,28 +2,28 @@ package org.apache.ode.test;
 
 public class DataHandling20Tests extends BPELTest {
 
-    public void testXPathNamespace1() throws Exception {
+    public void testXPathNamespace1() throws Throwable {
     	go("target/test-classes/bpel/2.0/TestXPathNamespace1");
     }
-    public void testXPathNamespace2() throws Exception {
+    public void testXPathNamespace2() throws Throwable {
     	go("target/test-classes/bpel/2.0/TestXPathNamespace2");
     }
-	public void testSubTreeAssign() throws Exception {
+	public void testSubTreeAssign() throws Throwable {
 		go("target/test-classes/bpel/2.0/TestSubTreeAssign");
 	}
-    public void testAssignActivity1() throws Exception {
+    public void testAssignActivity1() throws Throwable {
         go("src/test/resources/bpel/2.0/TestAssignActivity1");
     }
-    public void testAssignActivity2() throws Exception {
+    public void testAssignActivity2() throws Throwable {
         go("src/test/resources/bpel/2.0/TestAssignActivity2");
     }
-    public void testSimpleTypeParts() throws Exception {
+    public void testSimpleTypeParts() throws Throwable {
     	go("target/test-classes/bpel/2.0/TestSimpleTypeParts");
     }
-    public void testSimpleVariableType() throws Exception {
+    public void testSimpleVariableType() throws Throwable {
     	go("target/test-classes/bpel/2.0/TestSimpleVariableType");
     }
-    public void testXslTransform() throws Exception {
+    public void testXslTransform() throws Throwable {
         go("target/test-classes/bpel/2.0/TestXslTransform");
     }
 	
--- bpel-test/src/test/java/org/apache/ode/test/FaultHandling20Tests.java
+++ bpel-test/src/test/java/org/apache/ode/test/FaultHandling20Tests.java
@@ -1,13 +1,13 @@
 package org.apache.ode.test;
 
 public class FaultHandling20Tests extends BPELTest {
-	public void testFaultHandlers() throws Exception {
+	public void testFaultHandlers() throws Throwable {
 		go("target/test-classes/bpel/2.0/TestFaultHandlers");
 	}
-    public void testFaultWithVariable() throws Exception {
+    public void testFaultWithVariable() throws Throwable {
     	go("target/test-classes/bpel/2.0/TestFaultWithVariable");
     }
-	public void testCatchFaultInFaultHandler() throws Exception {
+	public void testCatchFaultInFaultHandler() throws Throwable {
 		go("target/test-classes/bpel/2.0/TestCatchFaultInFaultHandler");
 	}
 }
