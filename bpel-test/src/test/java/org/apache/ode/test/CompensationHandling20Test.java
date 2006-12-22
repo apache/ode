78c8180d2667e1d38e7046d463d4e38dca0dc05d
Lance Waterman <lwaterman@apache.org>
2006-12-22 00:07:28 +0000

    Fix Test name and add schema generation to build
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ode/trunk@489517 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-test/src/test/java/org/apache/ode/test/CompensationHandling20Test.java
+++ bpel-test/src/test/java/org/apache/ode/test/CompensationHandling20Test.java
@@ -1,6 +1,6 @@
 package org.apache.ode.test;
 
-public class CompensationHandling20Tests extends BPELTest {
+public class CompensationHandling20Test extends BPELTest {
 
 	public void testCompensationHandlers() throws Throwable {
 	 go("target/test-classes/bpel/2.0/TestCompensationHandlers");
