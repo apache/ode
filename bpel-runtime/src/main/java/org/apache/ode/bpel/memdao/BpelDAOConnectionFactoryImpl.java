a698f698e428ca79dd5704d9f6be32ce82a77357
Maciej Szefler <mszefler@apache.org>
2007-01-15 21:41:48 +0000

    Added shutdown method.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/ode/trunk@496505 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-runtime/src/main/java/org/apache/ode/bpel/memdao/BpelDAOConnectionFactoryImpl.java
+++ bpel-runtime/src/main/java/org/apache/ode/bpel/memdao/BpelDAOConnectionFactoryImpl.java
@@ -13,22 +13,23 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-
 /**
  * Connection factory for the in-memory state store.
  */
 public class BpelDAOConnectionFactoryImpl implements BpelDAOConnectionFactory {
-  private static final Map<QName, ProcessDaoImpl> __StateStore = new HashMap<QName, ProcessDaoImpl>();
+    private static final Map<QName, ProcessDaoImpl> __StateStore = new HashMap<QName, ProcessDaoImpl>();
 
+    public BpelDAOConnection getConnection() {
+        return new BpelDAOConnectionImpl(__StateStore);
+    }
 
-  public BpelDAOConnection getConnection() {
-    return new BpelDAOConnectionImpl(__StateStore);
-  }
+    /**
+     * @see org.apache.ode.bpel.dao.BpelDAOConnectionFactory#init(java.util.Properties)
+     */
+    public void init(Properties properties) {
+    }
 
-  /**
-   * @see org.apache.ode.bpel.dao.BpelDAOConnectionFactory#init(java.util.Properties)
-   */
-  public void init(Properties properties) {
-  }
+    public void shutdown() {
+    }
 
 }
