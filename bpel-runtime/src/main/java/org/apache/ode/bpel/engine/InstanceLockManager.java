13fb31dd77955307d3b10bf17cb4a6c41ddde8b7
Maciej Szefler <mszefler@apache.org>
2007-01-29 17:08:37 +0000

    Better logging for instance lock.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ode/trunk@501112 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-runtime/src/main/java/org/apache/ode/bpel/engine/InstanceLockManager.java
+++ bpel-runtime/src/main/java/org/apache/ode/bpel/engine/InstanceLockManager.java
@@ -24,6 +24,9 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * 
  * WARNING --- EXPERIMENTAL
@@ -34,11 +37,16 @@ import java.util.concurrent.locks.Lock;
  * @author Maciej Szefler - m s z e f l e r @ g m a i l . c o m
  */
 public class InstanceLockManager {
-
+    private static final Log __log = LogFactory.getLog(InstanceLockManager.class);
+    
     private final Lock _mutex = new java.util.concurrent.locks.ReentrantLock();
     private final Map<Long, InstanceInfo> _locks = new HashMap<Long,InstanceInfo> ();
     
     public void lock(Long iid, int time, TimeUnit tu) throws InterruptedException, TimeoutException {
+        String thrd = Thread.currentThread().toString();
+        if (__log.isDebugEnabled())
+            __log.debug(thrd + ": lock(iid=" + iid + ", time=" + time + tu+")");
+
         InstanceInfo li;
 
         _mutex.lock();
@@ -49,10 +57,18 @@ public class InstanceLockManager {
                 if (li == null) {
                     li = new InstanceInfo(iid, Thread.currentThread());
                     _locks.put(iid, li);
+                    if (__log.isDebugEnabled())
+                        __log.debug(thrd + ": lock(iid=" + iid + ", time=" + time + tu+")-->GRANTED");                    
                     return;
                 } else {
-                    if (!li.available.await(time, tu)) 
+                    if (__log.isDebugEnabled())
+                        __log.debug(thrd + ": lock(iid=" + iid + ", time=" + time + tu+")-->WAITING(held by " + li.acquierer + ")");
+                    
+                    if (!li.available.await(time, tu)) {
+                        if (__log.isDebugEnabled())
+                            __log.debug(thrd + ": lock(iid=" + iid + ", time=" + time + tu+")-->TIMEOUT (held by " + li.acquierer + ")");
                         throw new TimeoutException();
+                    }
                 }
             }
             
@@ -63,7 +79,10 @@ public class InstanceLockManager {
     }
     
     public void unlock(Long iid)  {
-        
+        String thrd = Thread.currentThread().toString();
+        if (__log.isDebugEnabled())
+            __log.debug(thrd + ": unlock(iid=" + iid + ")");        
+
         _mutex.lock();
         try {
             InstanceInfo li = _locks.get(iid);
