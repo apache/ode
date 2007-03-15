a29112367c79cb22ff509b2dae81c9f99a6ad96d
Matthieu Riou <mriou@apache.org>
2007-03-15 00:17:44 +0000

    Tests execution seems to work. Some tests fail but some work so I guess that's a good sign. Since we changed a bit the way mex works and things are scheduled, more had to be implemented there.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ode/trunk@518392 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-test/src/test/java/org/apache/ode/test/BPELTestAbstract.java
+++ bpel-test/src/test/java/org/apache/ode/test/BPELTestAbstract.java
@@ -32,6 +32,7 @@ import org.apache.ode.dao.jpa.BPELDAOConnectionFactoryImpl;
 import org.apache.ode.store.ProcessStoreImpl;
 import org.apache.ode.test.scheduler.TestScheduler;
 import org.apache.ode.utils.DOMUtils;
+import org.apache.ode.utils.GUID;
 import org.w3c.dom.Element;
 
 import javax.persistence.EntityManager;
@@ -41,28 +42,29 @@ import javax.xml.namespace.QName;
 import java.io.File;
 import java.util.Collection;
 import java.util.Properties;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 public abstract class BPELTestAbstract extends TestCase {
 
-	protected BpelServerImpl server;
+    protected BpelServerImpl server;
     protected ProcessStore store;
-	protected MessageExchangeContextImpl mexContext;
-	protected EntityManager em;
-	protected EntityManagerFactory emf;
+    protected MessageExchangeContextImpl mexContext;
+    protected EntityManager em;
+    protected EntityManagerFactory emf;
     protected TestScheduler scheduler;
     protected BpelDAOConnectionFactory _cf;
 
     @Override
-	protected void setUp() throws Exception {
-		server = new BpelServerImpl();
-		mexContext = new MessageExchangeContextImpl();
-		
-		
-		if ( Boolean.getBoolean("org.apache.ode.test.persitent")) {
-			emf = Persistence.createEntityManagerFactory("ode-unit-test-embedded");
-			em = emf.createEntityManager();
-			String pr = Persistence.PERSISTENCE_PROVIDER;
+    protected void setUp() throws Exception {
+        server = new BpelServerImpl();
+        mexContext = new MessageExchangeContextImpl();
+
+        if ( Boolean.getBoolean("org.apache.ode.test.persistent")) {
+            emf = Persistence.createEntityManagerFactory("ode-unit-test-embedded");
+            em = emf.createEntityManager();
+            String pr = Persistence.PERSISTENCE_PROVIDER;
             _cf = new BPELDAOConnectionFactoryImpl();
             server.setDaoConnectionFactory(_cf);
             scheduler = new TestScheduler() {
@@ -82,17 +84,18 @@ public abstract class BPELTestAbstract extends TestCase {
                     super.rollback();
                     em.getTransaction().rollback();
                 }
-                
+
             };
-		} else {
+        } else {
             _cf = new BpelDAOConnectionFactoryImpl();
             server.setDaoConnectionFactory(_cf);
             scheduler = new TestScheduler();
-		}
+        }
         server.setInMemDaoConnectionFactory(new BpelDAOConnectionFactoryImpl());
         server.setScheduler(scheduler);
-		server.setBindingContext(new BindingContextImpl());
-		server.setMessageExchangeContext(mexContext);
+        server.setBindingContext(new BindingContextImpl());
+        server.setMessageExchangeContext(mexContext);
+        scheduler.setJobProcessor(server);
         store = new ProcessStoreImpl(null, "jpa", true);
         store.registerListener(new ProcessStoreListener() {
             public void onProcessStoreEvent(ProcessStoreEvent event) {
@@ -103,196 +106,199 @@ public abstract class BPELTestAbstract extends TestCase {
             }
         });
         server.init();
-		server.start();
-	}
+        server.start();
+    }
 
-	@Override
-	protected void tearDown() throws Exception {
-		if ( em != null ) em.close();
-		if ( emf != null ) emf.close();
-		server.stop();
-	}
+    @Override
+    protected void tearDown() throws Exception {
+        if ( em != null ) em.close();
+        if ( emf != null ) emf.close();
+        server.stop();
+    }
 
-	protected void negative(String deployDir) throws Throwable {
-		try {
-			go(deployDir);
-		} catch (junit.framework.AssertionFailedError ex) {
-			return;
-		}
-		fail("Expecting test to fail");
-	}
+    protected void negative(String deployDir) throws Throwable {
+        try {
+            go(deployDir);
+        } catch (junit.framework.AssertionFailedError ex) {
+            return;
+        }
+        fail("Expecting test to fail");
+    }
 
-	protected void go(String deployDir) throws Throwable {
+    protected void go(String deployDir) throws Throwable {
 
-		/**
-		 * The deploy directory must contain at least one "test.properties"
-		 * file.
-		 *
-		 * The test.properties file identifies the service, operation and
-		 * messages to be sent to the BPEL engine.
-		 *
-		 * The deploy directory may contain more than one file in the form of
-		 * "testN.properties" where N represents a monotonic integer beginning
-		 * with 1.
-		 *
-		 */
+        /**
+         * The deploy directory must contain at least one "test.properties"
+         * file.
+         *
+         * The test.properties file identifies the service, operation and
+         * messages to be sent to the BPEL engine.
+         *
+         * The deploy directory may contain more than one file in the form of
+         * "testN.properties" where N represents a monotonic integer beginning
+         * with 1.
+         *
+         */
 
-		int propsFileCnt = 0;
-		File testPropsFile = new File(deployDir + "/test.properties");
+        int propsFileCnt = 0;
+        File testPropsFile = new File(deployDir + "/test.properties");
 
-		if (!testPropsFile.exists()) {
-			propsFileCnt++;
-			testPropsFile = new File(deployDir + "/test" + propsFileCnt
-					+ ".properties");
-			if (!testPropsFile.exists()) {
-				System.err.println("can't find " + testPropsFile.toString());
-			}
-		}
+        if (!testPropsFile.exists()) {
+            propsFileCnt++;
+            testPropsFile = new File(deployDir + "/test" + propsFileCnt
+                    + ".properties");
+            if (!testPropsFile.exists()) {
+                System.err.println("can't find " + testPropsFile.toString());
+            }
+        }
 
-		scheduler.begin();
-		try {
-			Collection<QName> procs =  store.deploy(new File(deployDir));
+        scheduler.begin();
+        try {
+            Collection<QName> procs =  store.deploy(new File(deployDir));
             for (QName procName : procs) {
                 server.register(store.getProcessConfiguration(procName));
             }
 
         } catch (ContextException bpelE) {
-			Properties testProps = new Properties();
-			testProps.load(testPropsFile.toURL().openStream());
-			String responsePattern = testProps.getProperty("response1");
+            Properties testProps = new Properties();
+            testProps.load(testPropsFile.toURL().openStream());
+            String responsePattern = testProps.getProperty("response1");
             bpelE.printStackTrace();
-			testResponsePattern(bpelE.getMessage(), responsePattern);
+            testResponsePattern(bpelE.getMessage(), responsePattern);
             return;
-		} catch ( Exception e ) {
-			e.printStackTrace();
-			fail();
-		}
-		scheduler.commit();
-        
-		while (testPropsFile.exists()) {
+        } catch ( Exception e ) {
+            e.printStackTrace();
+            fail();
+        }
+        scheduler.commit();
 
-			scheduler.begin();
-			
-			Properties testProps = new Properties();
-			testProps.load(testPropsFile.toURL().openStream());
+        while (testPropsFile.exists()) {
 
-			QName serviceId = new QName(testProps.getProperty("namespace"),
-					testProps.getProperty("service"));
-			String operation = testProps.getProperty("operation");
+            Properties testProps = new Properties();
+            testProps.load(testPropsFile.toURL().openStream());
 
-			MyRoleMessageExchange mex = server.getEngine()
-					.createMessageExchange("", serviceId, operation);
+            QName serviceId = new QName(testProps.getProperty("namespace"),
+                    testProps.getProperty("service"));
+            String operation = testProps.getProperty("operation");
 
-			/**
-			 * Each property file must contain at least one request/response
-			 * property tuple.
-			 *
-			 * The request/response tuple should be in the form
-			 *
-			 * requestN=<message>some XML input message</message>
-			 * responseN=.*some response message.*
-			 *
-			 * Where N is a monotonic integer beginning with 1.
-			 *
-			 * If a specific MEP is expected in lieu of a response message use:
-			 * responseN=ASYNC responseN=ONE_WAY responseN=COMPLETED_OK
-			 *
-			 */
-			try {
-				for (int i = 1; testProps.getProperty("request" + i) != null; i++) {
-	
-					String in = testProps.getProperty("request" + i);
-					String responsePattern = testProps.getProperty("response" + i);
-	
-					mexContext.clearCurrentResponse();
-	
-					Message request = mex.createMessage(null);
-	
-					Element elem = DOMUtils.stringToDOM(in);
-					request.setMessage(elem);
-	
-	
-					mex.invoke(request);
-	
-	
-					switch (mex.getStatus()) {
-					case RESPONSE:
-						testResponsePattern(mex.getResponse(), responsePattern);
-						// TODO: test for response fault
-						break;
-					case ASYNC:
-	
-						switch (mex.getMessageExchangePattern()) {
-						case REQUEST_ONLY:
-							if (!responsePattern.equals("ASYNC"))
-								fail();
-							break;
-						case REQUEST_RESPONSE:
-							testResponsePattern(mexContext.getCurrentResponse(),
-									responsePattern);
-						default:
-							break;
-						}
-	
-						break;
-					case COMPLETED_OK:
-						if (!responsePattern.equals("COMPLETED_OK"))
-							testResponsePattern(mexContext.getCurrentResponse(),
-									responsePattern);
-						break;
-					case FAULT:
-						// TODO: handle Fault
-						System.out.println("=> " + mex.getFaultExplanation());
-						fail();
-						break;
-					case COMPLETED_FAILURE:
-						// TODO: handle Failure
-						System.out.println("=> " + mex.getFaultExplanation());
-						fail();
-						break;
-					case COMPLETED_FAULT:
-						// TODO: handle Failure
-						System.out.println("=> " + mex.getFaultExplanation());
-						fail();
-						break;
-					case FAILURE:
-						// TODO: handle Faulure
-						System.out.println("=> " + mex.getFaultExplanation());
-						fail();
-						break;
-					default:
-						fail();
-						break;
-					}
-				}
-			} catch ( Throwable e ) {
-				e.printStackTrace();
-				scheduler.rollback();
-				throw e;
-			}
-			propsFileCnt++;
-			testPropsFile = new File(deployDir + "/test" + propsFileCnt
-					+ ".properties");
-			scheduler.commit();
-		}
-	}
+
+            /**
+             * Each property file must contain at least one request/response
+             * property tuple.
+             *
+             * The request/response tuple should be in the form
+             *
+             * requestN=<message>some XML input message</message>
+             * responseN=.*some response message.*
+             *
+             * Where N is a monotonic integer beginning with 1.
+             *
+             * If a specific MEP is expected in lieu of a response message use:
+             * responseN=ASYNC responseN=ONE_WAY responseN=COMPLETED_OK
+             *
+             */
+            for (int i = 1; testProps.getProperty("request" + i) != null; i++) {
+                MyRoleMessageExchange mex = null;
+                Future running = null;
+                String responsePattern = null;
+                try {
+                    scheduler.begin();
+
+                    mex = server.getEngine().createMessageExchange(new GUID().toString(), serviceId, operation);
+
+                    String in = testProps.getProperty("request" + i);
+                    responsePattern = testProps.getProperty("response" + i);
+
+                    mexContext.clearCurrentResponse();
+
+                    Message request = mex.createMessage(null);
+
+                    Element elem = DOMUtils.stringToDOM(in);
+                    request.setMessage(elem);
+
+
+                    running = mex.invoke(request);
+                    scheduler.commit();
+                } catch ( Throwable e ) {
+                    e.printStackTrace();
+                    scheduler.rollback();
+                    throw e;
+                }
+
+                running.get(10000, TimeUnit.MILLISECONDS);
+
+                switch (mex.getStatus()) {
+                    case RESPONSE:
+                        testResponsePattern(mex.getResponse(), responsePattern);
+                        // TODO: test for response fault
+                        break;
+                    case ASYNC:
+
+                        switch (mex.getMessageExchangePattern()) {
+                            case REQUEST_ONLY:
+                                if (!responsePattern.equals("ASYNC"))
+                                    fail();
+                                break;
+                            case REQUEST_RESPONSE:
+                                testResponsePattern(mexContext.getCurrentResponse(),
+                                        responsePattern);
+                            default:
+                                break;
+                        }
+
+                        break;
+                    case COMPLETED_OK:
+                        if (!responsePattern.equals("COMPLETED_OK"))
+                            testResponsePattern(mexContext.getCurrentResponse(),
+                                    responsePattern);
+                        break;
+                    case FAULT:
+                        // TODO: handle Fault
+                        System.out.println("=> " + mex.getFaultExplanation());
+                        fail();
+                        break;
+                    case COMPLETED_FAILURE:
+                        // TODO: handle Failure
+                        System.out.println("=> " + mex.getFaultExplanation());
+                        fail();
+                        break;
+                    case COMPLETED_FAULT:
+                        // TODO: handle Failure
+                        System.out.println("=> " + mex.getFaultExplanation());
+                        fail();
+                        break;
+                    case FAILURE:
+                        // TODO: handle Faulure
+                        System.out.println("=> " + mex.getFaultExplanation());
+                        fail();
+                        break;
+                    default:
+                        fail();
+                        break;
+                }
+            }
+            propsFileCnt++;
+            testPropsFile = new File(deployDir + "/test" + propsFileCnt
+                    + ".properties");
+        }
+    }
 
     private void testResponsePattern(Message response, String responsePattern) {
-		String resp = (response == null) ? "null" : DOMUtils
-				.domToString(response.getMessage());
-		testResponsePattern(resp, responsePattern);
-	}
+        String resp = (response == null) ? "null" : DOMUtils
+                .domToString(response.getMessage());
+        testResponsePattern(resp, responsePattern);
+    }
 
-	private void testResponsePattern(String resp, String responsePattern) {
-		boolean testValue = Pattern.compile(responsePattern, Pattern.DOTALL)
-				.matcher(resp).matches();
+    private void testResponsePattern(String resp, String responsePattern) {
+        boolean testValue = Pattern.compile(responsePattern, Pattern.DOTALL)
+                .matcher(resp).matches();
 
-		if (!testValue) {
-			System.out.println("=> Expected Response Pattern >> "
-					+ responsePattern);
-			System.out.println("=> Acutal Response >> " + resp);
-		}
-		assertTrue(testValue);
-	}
+        if (!testValue) {
+            System.out.println("=> Expected Response Pattern >> "
+                    + responsePattern);
+            System.out.println("=> Acutal Response >> " + resp);
+        }
+        assertTrue(testValue);
+    }
 
 }
