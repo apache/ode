30e58fb7e273d1f97b736473aeaf40b94b4c17f7
Alex Boisvert <boisvert@apache.org>
2007-03-27 21:43:17 +0000

    -Switch from JOTM transaction manager to Geronimo transaction manager -Switch from Minerva connection pool to Tranql connection pool
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/ode/trunk@523095 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-epr/src/main/java/org/apache/ode/il/EmbeddedGeronimoFactory.java
+++ bpel-epr/src/main/java/org/apache/ode/il/EmbeddedGeronimoFactory.java
@@ -17,27 +17,28 @@
  * under the License.
  */
 
-/*
- * Licensed under the X license (see http://www.x.org/terms.htm)
- */
-package org.opentools.minerva.pool;
+package org.apache.ode.il;
 
-/**
- * Optional interface for an object in an ObjectPool.  If the objects created
- * by the ObjcetFactory implement this, the pool will register as a listener
- * when an object is checked out, and deregister when the object is returned.
- * Then if the object sends a close or error event, the pool will return the
- * object to the pool without the client having to do so explicitly.
- *
- * @author Aaron Mulder (ammulder@alumni.princeton.edu)
- */
-public interface PooledObject {
-    /**
-     * Adds a new listener.
-     */
-    public void addPoolEventListener(PoolEventListener listener);
-    /**
-     * Removes a listener.
-     */
-    public void removePoolEventListener(PoolEventListener listener);
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.geronimo.transaction.manager.GeronimoTransactionManager;
+
+import javax.transaction.TransactionManager;
+
+public class EmbeddedGeronimoFactory {
+    private static final Log LOG = LogFactory.getLog(EmbeddedGeronimoFactory.class);
+
+    /* Public no-arg contructor is required */
+    public EmbeddedGeronimoFactory() {
+    }
+
+    public TransactionManager getTransactionManager() {
+        LOG.info("Using embedded Geronimo transaction manager");
+        try {
+            return new GeronimoTransactionManager();
+        } catch (Exception except) {
+            throw new IllegalStateException("Unable to instantiate Geronimo Transaction Manager", except);
+        }
+    }
+
 }
