01a50ba6d08c6f95f9dd6604dc5648f185e97a4e
Matthieu Riou <mriou@apache.org>
2007-03-28 20:32:04 +0000

    Fixed the basic test case (hello, wrong NS, if and timer).
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/ode/trunk@523453 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-test/src/test/java/org/apache/ode/test/BasicActivities20Test.java
+++ bpel-test/src/test/java/org/apache/ode/test/BasicActivities20Test.java
@@ -8,11 +8,8 @@ public class BasicActivities20Test extends BPELTestAbstract {
 	public void testNegativeTargetNS1() throws Throwable {
 		/**
 		 * Test for an invalid targetNamespace has been entered into the WSDL.
-		 * 
 		 * See JIRA ODE-67
-		 * 
 		 * Test for a specific exception message.
-		 * 
 		 */
 		go("target/test-classes/bpel/2.0/NegativeTargetNSTest1");
 	}
