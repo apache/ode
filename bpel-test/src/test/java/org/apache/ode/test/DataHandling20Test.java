961337062b2f01d785ab03e8ee79ad5b66059f6c
Assaf Arkin <assaf@apache.org>
2007-04-24 05:57:44 +0000

    Modified to use test processes form classpath instead of file system path gymnastics
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ode/trunk@531776 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-test/src/test/java/org/apache/ode/test/DataHandling20Test.java
+++ bpel-test/src/test/java/org/apache/ode/test/DataHandling20Test.java
@@ -21,31 +21,31 @@ package org.apache.ode.test;
 public class DataHandling20Test extends BPELTestAbstract {
 
     public void testXPathNamespace1() throws Throwable {
-    	go("target/test-classes/bpel/2.0/TestXPathNamespace1");
+        go("/bpel/2.0/TestXPathNamespace1");
     }
     public void testXPathNamespace2() throws Throwable {
-    	go("target/test-classes/bpel/2.0/TestXPathNamespace2");
+        go("/bpel/2.0/TestXPathNamespace2");
     }
-	public void testSubTreeAssign() throws Throwable {
-		go("target/test-classes/bpel/2.0/TestSubTreeAssign");
-	}
+    public void testSubTreeAssign() throws Throwable {
+        go("/bpel/2.0/TestSubTreeAssign");
+    }
     public void testAssignActivity1() throws Throwable {
-        go("src/test/resources/bpel/2.0/TestAssignActivity1");
+        go("/bpel/2.0/TestAssignActivity1");
     }
     public void testAssignActivity2() throws Throwable {
-        go("src/test/resources/bpel/2.0/TestAssignActivity2");
+        go("/bpel/2.0/TestAssignActivity2");
     }
     public void testAssignComplex() throws Throwable {
-        go("src/test/resources/bpel/2.0/TestAssignComplex");
+        go("/bpel/2.0/TestAssignComplex");
     }
     public void testSimpleTypeParts() throws Throwable {
-    	go("target/test-classes/bpel/2.0/TestSimpleTypeParts");
+        go("/bpel/2.0/TestSimpleTypeParts");
     }
     public void testSimpleVariableType() throws Throwable {
-    	go("target/test-classes/bpel/2.0/TestSimpleVariableType");
+        go("/bpel/2.0/TestSimpleVariableType");
     }
     public void testXslTransform() throws Throwable {
-        go("target/test-classes/bpel/2.0/TestXslTransform");
+        go("/bpel/2.0/TestXslTransform");
     }
 	
 }
