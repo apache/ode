31616f20b447ec7f3b6f52e217013548604b8818
Alex Boisvert <boisvert@apache.org>
2007-05-02 00:50:26 +0000

    Minor refactoring of SoapMessageConverter related to SOAPFactory
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/ode/trunk@534290 13f79535-47bb-0310-9956-ffa450edef68

--- axis2/src/test/java/org/apache/ode/axis2/SoapMessageConverterTest.java
+++ axis2/src/test/java/org/apache/ode/axis2/SoapMessageConverterTest.java
@@ -34,27 +34,36 @@ import org.apache.axiom.soap.SOAPEnvelope;
 import org.apache.axiom.soap.impl.builder.StAXSOAPModelBuilder;
 import org.apache.axiom.soap.impl.dom.soap11.SOAP11Factory;
 import org.apache.axis2.AxisFault;
+import org.apache.axis2.context.MessageContext;
 import org.apache.ode.axis2.util.SoapMessageConverter;
 import org.apache.ode.utils.DOMUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 public class SoapMessageConverterTest extends TestCase {
-    
-    Definition wsdl1,wsdlHW;
+
+    Definition wsdl1, wsdlHW;
+
     String wsdl1tns = "http://documentum.com/ws/2005/services";
-    QName repoService = new QName(wsdl1tns, "RepoAccessorService"); 
-    QName portTypeName = new QName(wsdl1tns,"RepoAccessor");
+
+    QName repoService = new QName(wsdl1tns, "RepoAccessorService");
+
+    QName portTypeName = new QName(wsdl1tns, "RepoAccessor");
+
     String portName = "RepoAccessor";
+
     SoapMessageConverter portmapper;
-    
+
     PortType portType, portTypeHW;
+
     Document req1bad;
+
     Document req1;
-    private Operation op1,opHello;
-    
+
+    private Operation op1, opHello;
+
     public SoapMessageConverterTest() throws Exception {
-        req1bad = DOMUtils.parse(getClass().getResourceAsStream("/testRequest1Bad.xml"));  
+        req1bad = DOMUtils.parse(getClass().getResourceAsStream("/testRequest1Bad.xml"));
         req1 = DOMUtils.parse(getClass().getResourceAsStream("/testRequest1.xml"));
         WSDLReader reader = WSDLFactory.newInstance().newWSDLReader();
         wsdl1 = reader.readWSDL(getClass().getResource("/test1.wsdl").toExternalForm());
@@ -62,93 +71,93 @@ public class SoapMessageConverterTest extends TestCase {
         op1 = portType.getOperation("getObjectId", null, null);
 
         wsdlHW = reader.readWSDL(getClass().getResource("/HelloWorld.wsdl").toExternalForm());
-        portTypeHW = wsdlHW.getPortType(new QName(wsdlHW.getTargetNamespace(),"HelloPortType"));
-        opHello = portTypeHW.getOperation("hello",null,null);
-}
-    
+        portTypeHW = wsdlHW.getPortType(new QName(wsdlHW.getTargetNamespace(), "HelloPortType"));
+        opHello = portTypeHW.getOperation("hello", null, null);
+    }
+
     public void setUp() throws Exception {
-        portmapper = new SoapMessageConverter(new SOAP11Factory(),
-                wsdl1, repoService, portName, true);
+        portmapper = new SoapMessageConverter(wsdl1, repoService, portName, true);
     }
-    
+
     public void tearDown() {
-        
+
     }
-    
+
     public void testBadPortName() {
         try {
-            new SoapMessageConverter(new SOAP11Factory(),
-                    wsdl1, repoService, "badPort", true);
+            new SoapMessageConverter(wsdl1, repoService, "badPort", true);
             fail("Should have thrown axis error.");
-        } catch(AxisFault af) {
-            ;//expected
+        } catch (AxisFault af) {
+            ;// expected
         }
     }
 
     public void testBadServiceName() {
         try {
-            new SoapMessageConverter(new SOAP11Factory(),
-                    wsdl1, new QName(wsdl1tns, "foobar"), portName, true);
+            new SoapMessageConverter(wsdl1, new QName(wsdl1tns, "foobar"), portName, true);
             fail("Should have thrown axis error.");
-        } catch(AxisFault af) {
-            ;//expected
+        } catch (AxisFault af) {
+            ;// expected
         }
     }
 
     public void testCreateSOAPRequest() throws Exception {
-        SOAPEnvelope env = new SOAP11Factory().createSOAPEnvelope();
-        portmapper.createSoapRequest(env, req1.getDocumentElement(),
-                portType.getOperation("getObjectId", null, null));
-        System.out.println(env);
+        MessageContext msgCtx = new MessageContext();
+        portmapper.createSoapRequest(msgCtx, req1.getDocumentElement(), portType.getOperation("getObjectId", null, null));
+        SOAPEnvelope env = msgCtx.getEnvelope();
+        System.out.println("testCreateSOAPRequest: " + env);
         QName elPartName = new QName(wsdl1tns, "getObjectId");
         assertNotNull(env.getBody());
         assertNotNull(env.getBody().getFirstElement());
         // doc-lit style, no part wrapper
-        assertEquals(elPartName,env.getBody().getFirstElement().getQName());
-        
+        assertEquals(elPartName, env.getBody().getFirstElement().getQName());
+
     }
-    
+
     public void testCreateSOAPRequestFail() throws Exception {
-        SOAPEnvelope env = new SOAP11Factory().createSOAPEnvelope();
+        MessageContext msgCtx = new MessageContext();
         try {
-        portmapper.createSoapRequest(env, req1bad.getDocumentElement(),
-                portType.getOperation("getObjectId", null, null));
-        fail("Should have caused an ex");
+            portmapper.createSoapRequest(msgCtx, req1bad.getDocumentElement(), portType.getOperation("getObjectId", null,
+                    null));
+            fail("Should have caused an ex");
         } catch (AxisFault af) {
             ; // expected
         }
     }
-    
+
     public void testGetSoapAction() throws Exception {
         assertEquals("getObjectIdAction", portmapper.getSoapAction("getObjectId"));
         assertEquals("", portmapper.getSoapAction("foo"));
     }
-    
+
     public void testParseRequest() throws Exception {
-        SOAPEnvelope env = new SOAP11Factory().createSOAPEnvelope();
-        portmapper.createSoapRequest(env, req1.getDocumentElement(),op1);
-        System.out.println(env);  
+        MessageContext msgCtx = new MessageContext();
+        portmapper.createSoapRequest(msgCtx, req1.getDocumentElement(), op1);
+        SOAPEnvelope env = msgCtx.getEnvelope();
+        System.out.println("testParseRequest: " + env);
         Element odeMsg = DOMUtils.stringToDOM("<message/>");
         portmapper.parseSoapRequest(odeMsg, env, op1);
-        System.out.println(DOMUtils.domToString(odeMsg));  
+        System.out.println("testParseRequest: " + DOMUtils.domToString(odeMsg));
         Element params = DOMUtils.findChildByName(odeMsg, new QName(null, "parameters"));
         assertNotNull(params);
-        Element hdr = DOMUtils.findChildByName(odeMsg,new QName("urn:ode.apache.org/axis2-il/header","DocumentumRequestHeader"));
+        Element hdr = DOMUtils.findChildByName(odeMsg, new QName("urn:ode.apache.org/axis2-il/header",
+                "DocumentumRequestHeader"));
         assertNotNull(hdr);
     }
-    
+
     /** Make sure hello world request parses correctly. */
     public void testHelloWorldRequest() throws Exception {
-        XMLStreamReader sr = XMLInputFactory.newInstance().createXMLStreamReader(getClass().getResourceAsStream("/HelloWorldRequest.soap"));
-        StAXSOAPModelBuilder builder = OMXMLBuilderFactory.createStAXSOAPModelBuilder(new SOAP11Factory(),sr);
+        SoapMessageConverter portmaper1 = new SoapMessageConverter(wsdlHW, new QName(wsdlHW
+                .getTargetNamespace(), "HelloService"), "HelloPort", false);
+
+        XMLStreamReader sr = XMLInputFactory.newInstance().createXMLStreamReader(
+                getClass().getResourceAsStream("/HelloWorldRequest.soap"));
+        StAXSOAPModelBuilder builder = OMXMLBuilderFactory.createStAXSOAPModelBuilder(new SOAP11Factory(), sr);
         SOAPEnvelope se = builder.getSOAPEnvelope();
-        SoapMessageConverter portmaper1 = new SoapMessageConverter(new SOAP11Factory(),
-                wsdlHW,new QName(wsdlHW.getTargetNamespace(),"HelloService"),"HelloPort",false);
-        
+
         Element msg = DOMUtils.stringToDOM("<message/>");
         portmaper1.parseSoapRequest(msg, se, opHello);
         System.out.println(DOMUtils.domToString(msg));
     }
 
 }
-
