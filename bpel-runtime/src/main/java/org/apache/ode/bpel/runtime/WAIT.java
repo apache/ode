9490f47039f0ffb60d8286ea468e914783a400e1
Matthieu Riou <mriou@apache.org>
2007-06-15 18:22:07 +0000

    Making it pretty.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/ode/trunk@547754 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-runtime/src/main/java/org/apache/ode/bpel/runtime/WAIT.java
+++ bpel-runtime/src/main/java/org/apache/ode/bpel/runtime/WAIT.java
@@ -37,97 +37,96 @@ import java.util.Date;
  * JacobRunnable that performs the work of the <code>&lt;wait&gt;</code> activity.
  */
 class WAIT extends ACTIVITY {
-	private static final long serialVersionUID = 1L;
-	private static final Log __log = LogFactory.getLog(WAIT.class);
+    private static final long serialVersionUID = 1L;
+    private static final Log __log = LogFactory.getLog(WAIT.class);
 
-  WAIT(ActivityInfo self, ScopeFrame scopeFrame, LinkFrame linkFrame) {
-    super(self, scopeFrame, linkFrame);
-  }
-
-  public final void run() {
-    
-  	Date dueDate = null;
-  	try{
-  		dueDate = getDueDate();
-  	}catch(FaultException e){
-  		__log.error("Fault while calculating due date: " 
-  				+ e.getQName() 
-  				+ "; Reason: " + e.getMessage());
-  		_self.parent.completed(createFault(e.getQName(), _self.o), CompensationHandler.emptySet());
-  		return;
-  	} catch (EvaluationException ee) {
-      String msg = "Unexpected error evaluating wait condition.";
-      __log.error(msg, ee);
-      throw new InvalidProcessException(msg,ee);
+    WAIT(ActivityInfo self, ScopeFrame scopeFrame, LinkFrame linkFrame) {
+        super(self, scopeFrame, linkFrame);
     }
-  	
 
-  	if(dueDate.getTime() > System.currentTimeMillis()){
-	    final TimerResponseChannel timerChannel = newChannel(TimerResponseChannel.class);
-	    getBpelRuntimeContext().registerTimer(timerChannel, dueDate);
-	    
-	    object(false, new TimerResponseChannelListener(timerChannel){
-         private static final long serialVersionUID = 3120518305645437327L;
-
-         public void onTimeout() {
-           _self.parent.completed(null, CompensationHandler.emptySet());
-         }
-
-        public void onCancel() {
-          _self.parent.completed(null, CompensationHandler.emptySet());
+    public final void run() {
+        Date dueDate = null;
+        try{
+            dueDate = getDueDate();
+        } catch(FaultException e){
+            __log.error("Fault while calculating due date: "
+                    + e.getQName()
+                    + "; Reason: " + e.getMessage());
+            _self.parent.completed(createFault(e.getQName(), _self.o), CompensationHandler.emptySet());
+            return;
+        } catch (EvaluationException ee) {
+            String msg = "Unexpected error evaluating wait condition.";
+            __log.error(msg, ee);
+            throw new InvalidProcessException(msg,ee);
         }
-      }.or(new TerminationChannelListener(_self.self) {
-        private static final long serialVersionUID = -2791243270691333946L;
 
-        public void terminate() {
-          _self.parent.completed(null, CompensationHandler.emptySet());
-          object(new TimerResponseChannelListener(timerChannel) {
-            private static final long serialVersionUID = 677746737897792929L;
 
-            public void onTimeout() {
-              //ignore
-            }
+        if(dueDate.getTime() > System.currentTimeMillis()){
+            final TimerResponseChannel timerChannel = newChannel(TimerResponseChannel.class);
+            getBpelRuntimeContext().registerTimer(timerChannel, dueDate);
 
-            public void onCancel() {
-              //ingore
-            }
-          });
+            object(false, new TimerResponseChannelListener(timerChannel){
+                private static final long serialVersionUID = 3120518305645437327L;
+
+                public void onTimeout() {
+                    _self.parent.completed(null, CompensationHandler.emptySet());
+                }
+
+                public void onCancel() {
+                    _self.parent.completed(null, CompensationHandler.emptySet());
+                }
+            }.or(new TerminationChannelListener(_self.self) {
+                private static final long serialVersionUID = -2791243270691333946L;
+
+                public void terminate() {
+                    _self.parent.completed(null, CompensationHandler.emptySet());
+                    object(new TimerResponseChannelListener(timerChannel) {
+                        private static final long serialVersionUID = 677746737897792929L;
+
+                        public void onTimeout() {
+                            //ignore
+                        }
+
+                        public void onCancel() {
+                            //ingore
+                        }
+                    });
+                }
+            }));
+        }else{
+            _self.parent.completed(null, null);
         }
-      }));
-    }else{
-      _self.parent.completed(null, null);
-    }
-  }
-
-
-  protected Date getDueDate() throws FaultException, EvaluationException {
-
-    OWait wait = (OWait)_self.o;
-
-    // Assume the data was well formed (we have a deadline or a duration)
-    assert wait.hasFor() || wait.hasUntil();
-
-    EvaluationContext evalCtx = getEvaluationContext();
-
-    Date dueDate = null;
-    if (wait.hasFor()) {
-      Calendar cal = Calendar.getInstance();
-      Duration duration = getBpelRuntimeContext().getExpLangRuntime().evaluateAsDuration(wait.forExpression, evalCtx);
-      duration.addTo(cal);
-      dueDate = cal.getTime();
-    } else if (wait.hasUntil()) {
-      Calendar cal = getBpelRuntimeContext().getExpLangRuntime().evaluateAsDate(wait.untilExpression, evalCtx);
-      dueDate = cal.getTime();
-    } else {
-      throw new AssertionError("Static checks failed to find bad WaitActivity!");
     }
 
-    // For now if we cannot evaluate a due date, we assume it is due now.
-    // TODO: BPEL-ISSUE: verify BPEL spec for proper handling of these errors
-    if (dueDate == null)
-      dueDate = new Date();
 
-    return dueDate;
-  }
+    protected Date getDueDate() throws FaultException, EvaluationException {
+
+        OWait wait = (OWait)_self.o;
+
+        // Assume the data was well formed (we have a deadline or a duration)
+        assert wait.hasFor() || wait.hasUntil();
+
+        EvaluationContext evalCtx = getEvaluationContext();
+
+        Date dueDate = null;
+        if (wait.hasFor()) {
+            Calendar cal = Calendar.getInstance();
+            Duration duration = getBpelRuntimeContext().getExpLangRuntime().evaluateAsDuration(wait.forExpression, evalCtx);
+            duration.addTo(cal);
+            dueDate = cal.getTime();
+        } else if (wait.hasUntil()) {
+            Calendar cal = getBpelRuntimeContext().getExpLangRuntime().evaluateAsDate(wait.untilExpression, evalCtx);
+            dueDate = cal.getTime();
+        } else {
+            throw new AssertionError("Static checks failed to find bad WaitActivity!");
+        }
+
+        // For now if we cannot evaluate a due date, we assume it is due now.
+        // TODO: BPEL-ISSUE: verify BPEL spec for proper handling of these errors
+        if (dueDate == null)
+            dueDate = new Date();
+
+        return dueDate;
+    }
 
 }
