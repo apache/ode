5b05c8ad2d3fe64eae52ed0dbb45a1cb0e3c46d3
Maciej Szefler <mszefler@apache.org>
2007-06-25 17:35:27 +0000

    Some missing BART files.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/ode/branches/bart@550561 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bpel-runtime/src/main/java/org/apache/ode/bpel/engine/MemBackedMessageImpl.java
@@ -0,0 +1,44 @@
+package org.apache.ode.bpel.engine;
+
+import javax.xml.namespace.QName;
+
+import org.apache.derby.impl.store.raw.data.SetReservedSpaceOperation;
+import org.apache.ode.bpel.iapi.BpelEngineException;
+import org.w3c.dom.Element;
+
+/**
+ * Message, stored in memory.
+ * 
+ * @author Maciej Szefler
+ * 
+ */
+class MemBackedMessageImpl extends MessageImpl {
+
+    private Element _msg;
+
+    private QName _type;
+
+    MemBackedMessageImpl(Element msg, QName type, boolean ro) {
+        _msg = msg;
+        _type = type;
+        if (ro)
+            makeReadOnly();
+    }
+
+    @Override
+    public Element getMessage() {
+        return _msg;
+    }
+
+    @Override
+    public QName getType() {
+        return _type;
+    }
+
+    @Override
+    public void setMessage(Element msg) {
+        checkWrite();
+        _msg = msg;
+    }
+
+}
---
+++ bpel-runtime/src/main/java/org/apache/ode/bpel/engine/ReliablePartnerRoleMessageExchangeImpl.java
@@ -0,0 +1,5 @@
+package org.apache.ode.bpel.engine;
+
+public class ReliablePartnerRoleMessageExchangeImpl extends PartnerRoleMessageExchangeImpl {
+
+}
