4c06ec16543617c7b4fc1fd2549b8c022813a0b6
Tammo van Lessen <vanto@apache.org>
2008-01-11 00:13:18 +0000

    p.getNumInstances() is now transient...
    git-svn-id: https://svn.apache.org/repos/asf/ode/trunk@610995 13f79535-47bb-0310-9956-ffa450edef68

--- dao-jpa-ojpa-derby/src/test/java/org/apache/ode/dao/jpa/test/SelectObjectTest.java
+++ dao-jpa-ojpa-derby/src/test/java/org/apache/ode/dao/jpa/test/SelectObjectTest.java
@@ -100,7 +100,6 @@ public class SelectObjectTest extends TestCase {
 		assertNotNull(p.getType());
 		assertNotNull(p.getProcessId());
 		assertEquals( p.getVersion() , 1 );
-		assertTrue( p.getNumInstances() > 0 );
 		
 		// Assert the CorrelatorDAO
 		CorrelatorDAO corr = p.getCorrelator(CORRELATOR_ID1);
