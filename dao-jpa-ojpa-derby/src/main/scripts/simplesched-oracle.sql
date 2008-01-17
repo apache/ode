b1814731ad3586453feb614e5bc96d932ad17295
Matthieu Riou <mriou@apache.org>
2008-01-17 16:02:43 +0000

    ODE-228 Simple scheduler script for Oracle.
    git-svn-id: https://svn.eu.apache.org/repos/asf/ode/branches/APACHE_ODE_1.1@612852 13f79535-47bb-0310-9956-ffa450edef68

--- dao-jpa-ojpa-derby/src/main/scripts/simplesched-oracle.sql
+++ dao-jpa-ojpa-derby/src/main/scripts/simplesched-oracle.sql
@@ -1,11 +1,4 @@
-CREATE TABLE ode_job (
-  jobid CHAR(64)  NOT NULL DEFAULT '',
-  ts BIGINT  NOT NULL DEFAULT 0,
-  nodeid char(64),
-  scheduled int  NOT NULL DEFAULT 0,
-  transacted int  NOT NULL DEFAULT 0,
-  details blob(4096),
-  PRIMARY KEY(jobid));
+CREATE TABLE ODE_JOB (jobid varchar2(64 char) DEFAULT '' NOT NULL, ts number(19,0) DEFAULT 0 NOT NULL, nodeid varchar2(64 char) NULL, scheduled number(12,0) DEFAULT 0 NOT NULL, transacted number(12,0) DEFAULT 0 NOT NULL, details BLOB, PRIMARY KEY(jobid));
 
 CREATE INDEX IDX_ODE_JOB_TS ON ode_job(ts);
 CREATE INDEX IDX_ODE_JOB_NODEID ON ode_job(nodeid);
