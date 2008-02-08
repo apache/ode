93d3e16d4906c3eefbda4d93ccc921abb6edac5a
Matthieu Riou <mriou@apache.org>
2008-02-08 17:59:05 +0000

    ODE-241 Fixing WSDL including XSD including XSD, kind of a monkey patch but works.
    git-svn-id: https://svn.eu.apache.org/repos/asf/ode/branches/APACHE_ODE_1.1@619959 13f79535-47bb-0310-9956-ffa450edef68

---
+++ axis2-war/src/test/java/org/apache/ode/axis2/XSDReferencesDeployTest.java
@@ -0,0 +1,20 @@
+package org.apache.ode.axis2;
+
+/**
+ * @author Matthieu Riou <mriou@apache.org>
+ */
+public class XSDReferencesDeployTest extends Axis2TestBase {
+
+    protected void setUp() throws Exception {
+        start();
+    }
+
+    public void testSimpleFaultCatch() throws Exception {
+        server.deployService(DummyService.class.getCanonicalName());
+        // If we have a bug, this will throw an exception, no need to assert
+        server.deployProcess("XSDReferences");
+
+        server.undeployProcess("XSDReferences");
+    }
+
+}
