c1bcfde43c6293d82970990cdd8979d5a085d372
Matthieu Riou <mriou@apache.org>
2008-02-28 23:13:04 +0000

    Tomcat transaction manager lookup.
    git-svn-id: https://svn.apache.org/repos/asf/ode/branches/APACHE_ODE_1.1@632158 13f79535-47bb-0310-9956-ffa450edef68

---
+++ axis2/src/main/java/org/apache/ode/axis2/util/TomcatFactory.java
@@ -0,0 +1,32 @@
+package org.apache.ode.axis2.util;
+
+import javax.transaction.TransactionManager;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ * @author Matthieu Riou <mriou@apache.org>
+ */
+public class TomcatFactory {
+
+    public static final String JNDI_LOOKUP_PATH = "java:comp/UserTransaction";
+
+    /* Public no-arg contructor is required */
+    public TomcatFactory() {
+    }
+
+    public TransactionManager getTransactionManager() {
+        InitialContext ctx;
+        try {
+           ctx = new InitialContext();
+        } catch (NamingException except) {
+            throw new RuntimeException( "Can't create InitialContext", except);
+        }
+        try {
+            return (TransactionManager) ctx.lookup(JNDI_LOOKUP_PATH);
+        } catch (NamingException except) {
+            throw new RuntimeException( "Error while looking up TransactionManager at " + JNDI_LOOKUP_PATH, except);
+        }
+    }
+
+}
