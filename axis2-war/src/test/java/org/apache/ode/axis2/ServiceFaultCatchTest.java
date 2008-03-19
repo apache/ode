d50537f54b1ff06b9a77a4f9ac52f4bad923bac8
Matthieu Riou <mriou@apache.org>
2008-03-19 03:12:11 +0000

    ODE-247 Test fix when running more than once.
    git-svn-id: https://svn.eu.apache.org/repos/asf/ode/trunk@638690 13f79535-47bb-0310-9956-ffa450edef68

--- axis2-war/src/test/java/org/apache/ode/axis2/ServiceFaultCatchTest.java
+++ axis2-war/src/test/java/org/apache/ode/axis2/ServiceFaultCatchTest.java
@@ -17,14 +17,21 @@ public class ServiceFaultCatchTest extends Axis2TestBase {
     }
 
     public void testSimpleFaultCatch() throws Exception {
+        String bundleName = "TestStructuredFault";
+        // deploy the required service
         server.deployService(DummyService.class.getCanonicalName());
-        server.deployProcess("TestStructuredFault");
+        // Deploy the process if necessary.
+        // Actually undeployed process are automatically re-deployed during ODE Server start-up.
+        if(!server._ode.getProcessStore().getPackages().contains(bundleName)){
+            server.deployProcess(bundleName);
+        }
+        try {
+            String response = server.sendRequestFile("http://localhost:8080/processes/helloWorld",
+                    bundleName, "testRequest.soap");
 
-        String response = server.sendRequestFile("http://localhost:8080/processes/helloWorld",
-                "TestStructuredFault", "testRequest.soap");
-        
-        assert(response.indexOf("Something went wrong. Fortunately, it was meant to be.") >= 0);
-
-        server.undeployProcess("TestStructuredFault");
+            assert(response.indexOf("Something went wrong. Fortunately, it was meant to be.") >= 0);
+        } finally {
+            server.undeployProcess(bundleName);
+        }
     }
 }
