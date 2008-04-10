6add6a82957d33881795c3d5f49efa67395ed99e
Matthieu Riou <mriou@apache.org>
2008-04-10 23:14:19 +0000

    Several optimizations on Hibernate DAOs. Avoid loading a whole collection just to insert an element. Simple variables transactional cache.
    git-svn-id: https://svn.apache.org/repos/asf/ode/branches/APACHE_ODE_1.1@647004 13f79535-47bb-0310-9956-ffa450edef68

--- dao-hibernate/src/main/java/org/apache/ode/daohib/bpel/XmlDataDaoImpl.java
+++ dao-hibernate/src/main/java/org/apache/ode/daohib/bpel/XmlDataDaoImpl.java
@@ -38,130 +38,130 @@ import org.w3c.dom.Text;
  * Hibernate-based {@link XmlDataDAO} implementation.
  */
 class XmlDataDaoImpl extends HibernateDao implements XmlDataDAO {
-  private static final String QUERY_PROPERTY =
-          "from " + HVariableProperty.class.getName() +
-          " as p where p.xmlData.id = ? and p.name = ?";
+    private static final String QUERY_PROPERTY =
+            "from " + HVariableProperty.class.getName() +
+                    " as p where p.xmlData.id = ? and p.name = ?";
 
-	private HXmlData _data;
-  private Node _node;
+    private HXmlData _data;
+    private Node _node;
 
-  /**
-	 * @param hobj
-	 */
-	public XmlDataDaoImpl(SessionManager sm, HXmlData hobj) {
-		super(sm, hobj);
-    _data = hobj;
-	}
-	/**
-	 * @see org.apache.ode.bpel.dao.XmlDataDAO#isNull()
-	 */
-	public boolean isNull() {
-		return _data.getData() == null;
-	}
-  
-	/**
-	 * @see org.apache.ode.bpel.dao.XmlDataDAO#get()
-	 */
-	public Node get() {
-		if(_node == null){
-			_node = prepare();
+    /**
+     * @param hobj
+     */
+    public XmlDataDaoImpl(SessionManager sm, HXmlData hobj) {
+        super(sm, hobj);
+        _data = hobj;
     }
-    return _node;
-	}
-	/**
-	 * @see org.apache.ode.bpel.dao.XmlDataDAO#remove()
-	 */
-	public void remove() {
-		
-	}
-	/**
-	 * @see org.apache.ode.bpel.dao.XmlDataDAO#set(org.w3c.dom.Node)
-	 */
-	public void set(Node val) {
-		_node = val;
-    _data.setSimpleType(!(val instanceof Element));
-    if (_data.getData() != null)
-      _sm.getSession().delete(_data.getData());
-    HLargeData ld = new HLargeData();
-    if(_data.isSimpleType()) {
-      ld.setBinary(_node.getNodeValue().getBytes());
-      _data.setData(ld);
-    } else {
-      ld.setBinary(DOMUtils.domToString(_node).getBytes());
-      _data.setData(ld);
+    /**
+     * @see org.apache.ode.bpel.dao.XmlDataDAO#isNull()
+     */
+    public boolean isNull() {
+        return _data.getData() == null;
     }
-    getSession().save(ld);
-    getSession().update(_data);
-  }
-	/**
-	 * @see org.apache.ode.bpel.dao.XmlDataDAO#getProperty(java.lang.String)
-	 */
-	public String getProperty(String propertyName) {
-		
-    HVariableProperty p = _getProperty(propertyName);
-    return p == null
-      ? null
-      : p.getValue();
-	}
-	
-	/**
-	 * @see org.apache.ode.bpel.dao.XmlDataDAO#setProperty(java.lang.String, java.lang.String)
-	 */
-	public void setProperty(String pname, String pvalue) {
-		HVariableProperty p = _getProperty(pname);
-    if(p == null){
-    	p = new HVariableProperty(_data, pname, pvalue);
-      getSession().save(p);
-      _data.getProperties().add(p);
-    }else{
-      p.setValue(pvalue);
-      getSession().update(p);
-    }
-  }
-  
-  /**
-   * @see org.apache.ode.bpel.dao.XmlDataDAO#getScopeDAO()
-   */
-  public ScopeDAO getScopeDAO() {
-    return new ScopeDaoImpl(_sm,_data.getScope());
-  }
-  
-  private HVariableProperty _getProperty(String propertyName){
-    Iterator iter;
-    Query qry = getSession().createQuery(QUERY_PROPERTY);
-    qry.setLong(0, _data.getId());
-    qry.setString(1, propertyName);
-    iter = qry.iterate();
-    return iter.hasNext()
-            ? (HVariableProperty)iter.next()
-            : null;
-  }
-  
-  private Node prepare(){
-    if(_data.getData() == null)
-      return null;
-    String data = _data.getData().getText();
-    if(_data.isSimpleType()){
-      Document d = DOMUtils.newDocument();
-      // we create a dummy wrapper element
-      // prevents some apps from complaining
-      // when text node is not actual child of document
-      Element e = d.createElement("text-node-wrapper");
-      Text tnode = d.createTextNode(data);
-      d.appendChild(e);
-      e.appendChild(tnode);
-      return tnode;
-    }else{
-      try{
-        return DOMUtils.stringToDOM(data);
-      }catch(Exception e){
-        throw new RuntimeException(e);
-      }
-    }
-  }
 
-  public String getName() {
-    return _data.getName();
-  }
-	
+    /**
+     * @see org.apache.ode.bpel.dao.XmlDataDAO#get()
+     */
+    public Node get() {
+        if(_node == null){
+            _node = prepare();
+        }
+        return _node;
+    }
+    /**
+     * @see org.apache.ode.bpel.dao.XmlDataDAO#remove()
+     */
+    public void remove() {
+
+    }
+    /**
+     * @see org.apache.ode.bpel.dao.XmlDataDAO#set(org.w3c.dom.Node)
+     */
+    public void set(Node val) {
+        _node = val;
+        _data.setSimpleType(!(val instanceof Element));
+        if (_data.getData() != null) _sm.getSession().delete(_data.getData());
+        
+        HLargeData ld = new HLargeData();
+        if(_data.isSimpleType()) {
+            ld.setBinary(_node.getNodeValue().getBytes());
+            _data.setData(ld);
+        } else {
+            ld.setBinary(DOMUtils.domToString(_node).getBytes());
+            _data.setData(ld);
+        }
+        getSession().save(ld);
+        getSession().saveOrUpdate(_data);
+    }
+    /**
+     * @see org.apache.ode.bpel.dao.XmlDataDAO#getProperty(java.lang.String)
+     */
+    public String getProperty(String propertyName) {
+
+        HVariableProperty p = _getProperty(propertyName);
+        return p == null
+                ? null
+                : p.getValue();
+    }
+
+    /**
+     * @see org.apache.ode.bpel.dao.XmlDataDAO#setProperty(java.lang.String, java.lang.String)
+     */
+    public void setProperty(String pname, String pvalue) {
+        HVariableProperty p = _getProperty(pname);
+        if(p == null){
+            p = new HVariableProperty(_data, pname, pvalue);
+            getSession().save(p);
+//            _data.addProperty(p);
+        }else{
+            p.setValue(pvalue);
+            getSession().update(p);
+        }
+    }
+
+    /**
+     * @see org.apache.ode.bpel.dao.XmlDataDAO#getScopeDAO()
+     */
+    public ScopeDAO getScopeDAO() {
+        return new ScopeDaoImpl(_sm,_data.getScope());
+    }
+
+    private HVariableProperty _getProperty(String propertyName){
+        Iterator iter;
+        Query qry = getSession().createQuery(QUERY_PROPERTY);
+        qry.setLong(0, _data.getId());
+        qry.setString(1, propertyName);
+        iter = qry.iterate();
+        return iter.hasNext()
+                ? (HVariableProperty)iter.next()
+                : null;
+    }
+
+    private Node prepare(){
+        if(_data.getData() == null)
+            return null;
+        String data = _data.getData().getText();
+        if(_data.isSimpleType()){
+            Document d = DOMUtils.newDocument();
+            // we create a dummy wrapper element
+            // prevents some apps from complaining
+            // when text node is not actual child of document
+            Element e = d.createElement("text-node-wrapper");
+            Text tnode = d.createTextNode(data);
+            d.appendChild(e);
+            e.appendChild(tnode);
+            return tnode;
+        }else{
+            try{
+                return DOMUtils.stringToDOM(data);
+            }catch(Exception e){
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public String getName() {
+        return _data.getName();
+    }
+
 }
