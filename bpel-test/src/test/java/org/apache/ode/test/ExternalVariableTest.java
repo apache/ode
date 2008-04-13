63c0350b15bbc65a2ea872f6cbbf65dee60a36bd
Alex Boisvert <boisvert@apache.org>
2008-04-13 13:23:15 +0000

    Oops uncomment previous test cases
    git-svn-id: https://svn.apache.org/repos/asf/ode/trunk@647570 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-test/src/test/java/org/apache/ode/test/ExternalVariableTest.java
+++ bpel-test/src/test/java/org/apache/ode/test/ExternalVariableTest.java
@@ -46,7 +46,6 @@ public class ExternalVariableTest extends BPELTestAbstract {
         conn.close();
     }
 
-    /*
     @Test
     public void testHelloWorld2() throws Throwable {
         go("/bpel/2.0/ExtVar");
@@ -56,10 +55,9 @@ public class ExternalVariableTest extends BPELTestAbstract {
     public void testExtVar2() throws Throwable {
         go("/bpel/2.0/ExtVar2");
     }
-    */
 
     @Test
-    public void testExtVar2() throws Throwable {
+    public void testExtVar3() throws Throwable {
         go("/bpel/2.0/ExtVar3");
     }
 }
