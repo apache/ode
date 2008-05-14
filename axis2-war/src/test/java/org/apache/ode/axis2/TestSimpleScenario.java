a5047e183917c9719e6555f8cf9d98269e730f17
Matthieu Riou <mriou@apache.org>
2008-05-14 21:56:56 +0000

    More tests for basic process-to-process scenarios. Request-only P2P with a callback is broken right now.
    git-svn-id: https://svn.eu.apache.org/repos/asf/ode/trunk@656446 13f79535-47bb-0310-9956-ffa450edef68

---
+++ axis2-war/src/test/java/org/apache/ode/axis2/TestSimpleScenario.java
@@ -0,0 +1,40 @@
+package org.apache.ode.axis2;
+
+public class TestSimpleScenario extends Axis2TestBase {
+
+    protected void setUp() throws Exception {
+        start();
+    }
+
+    public void testDynPartner() throws Exception {
+        String bundleName = "TestDynPartner";
+        if(!server._ode.getProcessStore().getPackages().contains(bundleName)){
+            server.deployProcess(bundleName);
+        }
+        try {
+            String response = server.sendRequestFile("http://localhost:8080/ode/processes/DynMainService",
+                    bundleName, "testRequest.soap");
+
+            assertTrue(response.indexOf("OK") > 0);
+        } finally {
+            server.undeployProcess(bundleName);
+        }
+
+    }
+
+    public void testMagicSession() throws Exception {
+        String bundleName = "TestMagicSession";
+        if(!server._ode.getProcessStore().getPackages().contains(bundleName)){
+            server.deployProcess(bundleName);
+        }
+        try {
+            String response = server.sendRequestFile("http://localhost:8080/ode/processes/MSMainExecuteService",
+                    bundleName, "testRequest.soap");
+
+            assertTrue(response.indexOf("OK") > 0);
+        } finally {
+            server.undeployProcess(bundleName);
+        }
+
+    }
+}
