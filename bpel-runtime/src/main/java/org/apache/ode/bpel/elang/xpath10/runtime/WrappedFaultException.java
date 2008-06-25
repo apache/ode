177351c3ad16da3eab82c5380fec15854906a17a
Matthieu Riou <mriou@apache.org>
2008-06-25 15:40:42 +0000

    Can't reinitialize cause, have to use getFaultException() instead.
    git-svn-id: https://svn.eu.apache.org/repos/asf/ode/trunk@671583 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-runtime/src/main/java/org/apache/ode/bpel/elang/xpath10/runtime/WrappedFaultException.java
+++ bpel-runtime/src/main/java/org/apache/ode/bpel/elang/xpath10/runtime/WrappedFaultException.java
@@ -27,45 +27,43 @@ import org.jaxen.UnresolvableException;
  *  Wrap a fault in a jaxen exception
  */
 public interface WrappedFaultException {
-	public FaultException getFaultException();
-	
-	/**
-	 * Jaxenized  {@link FaultException}; Jaxen requires us to throw only exceptions
-	 * extending its {@link UnresolvableVariableException} so we comply.
-	 */
-	static class JaxenUnresolvableException extends UnresolvableException implements WrappedFaultException{
-    private static final long serialVersionUID = 6266231885976155458L;
-    
-    FaultException _cause;
-	  public JaxenUnresolvableException(FaultException e) {
-	  	super("var");
-	    assert e != null;
-	    initCause(e);
-	    _cause = e;
-	  }
+    public FaultException getFaultException();
 
-	  public FaultException getFaultException() {
-	    return _cause;
-	  }
-	}
-	
-	/**
-	 * Jaxenized  {@link FaultException}; Jaxen requires us to throw only exceptions
-	 * extending its {@link FunctionCallException} so we comply.
-	 */
-	static class JaxenFunctionException extends FunctionCallException implements WrappedFaultException{
-    private static final long serialVersionUID = -1915683768194623625L;
-    FaultException _cause;
-    
-	  public JaxenFunctionException(FaultException e) {
-	    super(e);
-	    assert e != null;
-	    initCause(e);
-	    _cause = e;
-	  }
+    /**
+     * Jaxenized  {@link FaultException}; Jaxen requires us to throw only exceptions
+     * extending its {@link UnresolvableVariableException} so we comply.
+     */
+    static class JaxenUnresolvableException extends UnresolvableException implements WrappedFaultException{
+        private static final long serialVersionUID = 6266231885976155458L;
 
-	  public FaultException getFaultException() {
-	    return _cause;
-	  }
-	}
+        FaultException _cause;
+        public JaxenUnresolvableException(FaultException e) {
+            super("var");
+            assert e != null;
+            _cause = e;
+        }
+
+        public FaultException getFaultException() {
+            return _cause;
+        }
+    }
+
+    /**
+     * Jaxenized  {@link FaultException}; Jaxen requires us to throw only exceptions
+     * extending its {@link FunctionCallException} so we comply.
+     */
+    static class JaxenFunctionException extends FunctionCallException implements WrappedFaultException{
+        private static final long serialVersionUID = -1915683768194623625L;
+        FaultException _cause;
+
+        public JaxenFunctionException(FaultException e) {
+            super(e);
+            assert e != null;
+            _cause = e;
+        }
+
+        public FaultException getFaultException() {
+            return _cause;
+        }
+    }
 }
