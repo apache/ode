b35fec41b1352399cf5cf9577a5067d5a75467b7
Alexis Midon <midon@apache.org>
2008-06-27 02:27:16 +0000

    ODE-307: Allows missing parts in urlReplacement pattern
    git-svn-id: https://svn.eu.apache.org/repos/asf/ode/branches/APACHE_ODE_1.1@672080 13f79535-47bb-0310-9956-ffa450edef68

--- axis2/src/main/java/org/apache/ode/axis2/httpbinding/HttpBindingValidator.java
+++ axis2/src/main/java/org/apache/ode/axis2/httpbinding/HttpBindingValidator.java
@@ -141,8 +141,10 @@ public class HttpBindingValidator {
                 String name = (String) it.next();
                 Pattern p = Pattern.compile(".*(\\(" + name + "\\)).*");
                 Matcher m = p.matcher(locationUri);
-                // is there a regex to do both validations in one?
-                if (!m.matches() || locationUri.split("(\\(" + name + "\\))", -1).length != 2) {
+                // WSLD spec requires that all message parts must be exactly once in the url pattern.
+                // However ODE relaxes this.
+                // The only test is to make sure a part is not mentionned more than once
+                if (m.matches() && locationUri.split("(\\(" + name + "\\))", -1).length != 2) {
                     throw new IllegalArgumentException(httpMsgs.msgInvalidURIPattern(binding, bindingOperation, locationUri));
                 }
             }
