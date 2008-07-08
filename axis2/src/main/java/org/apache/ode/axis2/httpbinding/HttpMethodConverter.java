6b80126050eb07668c4eb1a130fc2c9f95e3b660
Alexis Midon <midon@apache.org>
2008-07-08 21:52:37 +0000

    use the same syntax as in the HTTP standard
    git-svn-id: https://svn.apache.org/repos/asf/ode/trunk@674980 13f79535-47bb-0310-9956-ffa450edef68

--- axis2/src/main/java/org/apache/ode/axis2/httpbinding/HttpMethodConverter.java
+++ axis2/src/main/java/org/apache/ode/axis2/httpbinding/HttpMethodConverter.java
@@ -363,7 +363,7 @@ public class HttpMethodConverter {
         }
 
         // make the status line information available as a single element
-        odeMessage.setHeaderPart("StatusLine", HttpClientHelper.statusLineToElement(method.getStatusLine()));
+        odeMessage.setHeaderPart("Status-Line", HttpClientHelper.statusLineToElement(method.getStatusLine()));
     }
 
 
