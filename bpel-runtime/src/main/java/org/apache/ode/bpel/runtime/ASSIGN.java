fc130fddc7bddfe381a0f43755e665f46a0df6be
Matthieu Riou <mriou@apache.org>
2008-08-18 23:17:45 +0000

    Small fix for direct URL assignment on partner links.
    git-svn-id: https://svn.apache.org/repos/asf/ode/trunk@686894 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-runtime/src/main/java/org/apache/ode/bpel/runtime/ASSIGN.java
+++ bpel-runtime/src/main/java/org/apache/ode/bpel/runtime/ASSIGN.java
@@ -434,10 +434,14 @@ class ASSIGN extends ACTIVITY {
             Document doc = DOMUtils.newDocument();
             Element serviceRef = doc.createElementNS(Namespaces.WSBPEL2_0_FINAL_SERVREF, "service-ref");
             doc.appendChild(serviceRef);
-            NodeList children = rvalue.getChildNodes();
-            for (int m = 0; m < children.getLength(); m++) {
-                Node child = children.item(m);
-                serviceRef.appendChild(doc.importNode(child, true));
+            if (rvalue.getNodeType() == Node.TEXT_NODE) {
+                serviceRef.appendChild(doc.importNode(rvalue, true));
+            } else {
+                NodeList children = rvalue.getChildNodes();
+                for (int m = 0; m < children.getLength(); m++) {
+                    Node child = children.item(m);
+                    serviceRef.appendChild(doc.importNode(child, true));
+                }
             }
             rvalue = serviceRef;
         }
