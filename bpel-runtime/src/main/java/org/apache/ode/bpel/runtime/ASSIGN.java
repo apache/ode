9cc2a5df103e9c6dce58901ba4a18f8b33879af8
Matthieu Riou <mriou@apache.org>
2008-09-10 21:49:00 +0000

    Explicit error when assigning an attribute to an endpoint.
    git-svn-id: https://svn.eu.apache.org/repos/asf/ode/branches/APACHE_ODE_1.X@694012 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-runtime/src/main/java/org/apache/ode/bpel/runtime/ASSIGN.java
+++ bpel-runtime/src/main/java/org/apache/ode/bpel/runtime/ASSIGN.java
@@ -446,6 +446,10 @@ class ASSIGN extends ACTIVITY {
 	}
 	
 	private void replaceEndpointRefence(PartnerLinkInstance plval, Node rvalue) throws FaultException {
+      if (rvalue.getNodeType() == Node.ATTRIBUTE_NODE)
+          throw new FaultException(getOAsssign().getOwner().constants.qnMismatchedAssignmentFailure,
+                "Can't assign an attribute to an endpoint, you probably want to select the attribute text.");
+
         // Eventually wrapping with service-ref element if we've been directly assigned some
         // value that isn't wrapped.
         if (rvalue.getNodeType() == Node.TEXT_NODE ||
