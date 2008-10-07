a838ff9cdc9791cafa7396c528d9e12781647afc
Alex Boisvert <boisvert@apache.org>
2008-10-07 07:30:26 +0000

    Use BigDecimal to parse integer value since XPath can generate values like 123.0
    git-svn-id: https://svn.apache.org/repos/asf/ode/branches/APACHE_ODE_1.X@702370 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-runtime/src/main/java/org/apache/ode/bpel/extvar/jdbc/DbExternalVariable.java
+++ bpel-runtime/src/main/java/org/apache/ode/bpel/extvar/jdbc/DbExternalVariable.java
@@ -462,7 +462,7 @@ class DbExternalVariable {
                         return 1;
                     if (v.equals("false"))
                         return 0;
-					return Long.valueOf(val);
+					return java.math.BigDecimal(val).longValue;
                 } else if (isReal())
 					return Double.valueOf(val);
                 else if (isBoolean()) {
