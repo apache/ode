831591fee8505667dd79787fe780f99b6551bc79
Alexis Midon <midon@apache.org>
2009-03-06 23:21:56 +0000

    restart server before each test
    git-svn-id: https://svn.eu.apache.org/repos/asf/ode/trunk@751132 13f79535-47bb-0310-9956-ffa450edef68

--- axis2-war/src/test/java/org/apache/ode/axis2/rampart/basic/SecuredServicesTest.java
+++ axis2-war/src/test/java/org/apache/ode/axis2/rampart/basic/SecuredServicesTest.java
@@ -20,8 +20,8 @@
 package org.apache.ode.axis2.rampart.basic;
 
 import static org.testng.AssertJUnit.assertTrue;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
@@ -57,13 +57,13 @@ public class SecuredServicesTest extends Axis2TestBase {
     }
 
 
-    @BeforeClass
+    @BeforeMethod
     protected void setUp() throws Exception {
         // mind the annotation above: start the server only once for all tests
         startServer(testDir, "webapp/WEB-INF/conf/axis2.xml");
     }
 
-    @AfterClass
+    @AfterMethod
     protected void tearDown() throws Exception {
         // mind the annotation above: start the server only once for all tests
         super.tearDown();
