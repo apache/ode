81e0f7ef6de62f1c83f3eed3ee7801b71806a208
Sean Ahn <seanahn@apache.org>
2009-06-01 19:07:31 +0000

    Removed tabs
    git-svn-id: https://svn.apache.org/repos/asf/ode/branches/APACHE_ODE_1.X@780786 13f79535-47bb-0310-9956-ffa450edef68

--- dao-hibernate/src/main/java/org/apache/ode/daohib/bpel/ProcessManagementDaoImpl.java
+++ dao-hibernate/src/main/java/org/apache/ode/daohib/bpel/ProcessManagementDaoImpl.java
@@ -32,37 +32,37 @@ import org.hibernate.Query;
 import org.hibernate.transform.ResultTransformer;
 
 public class ProcessManagementDaoImpl extends HibernateDao implements ProcessManagementDAO {
-	protected ProcessManagementDaoImpl(SessionManager sessionManager) {
-		super(sessionManager, null);
-	}
+    protected ProcessManagementDaoImpl(SessionManager sessionManager) {
+        super(sessionManager, null);
+    }
 
-	@SuppressWarnings("unchecked")
-	public Object[] findFailedCountAndLastFailedDateForProcessId(BpelDAOConnection conn, String status, String processId) {
-		Object[] results = new Object[] {0, null};
-		
-		Query query = getSession().getNamedQuery(HProcessInstance.COUNT_FAILED_INSTANCES_BY_PROCESS_IDS_AND_STATES);
-		query.setParameterList("states", new InstanceFilter("status=" + status).convertFilterState());
-		query.setParameterList("processIds", new String[] {processId});
-		query.setResultTransformer(new ResultTransformer() {
-			private static final long serialVersionUID = 8034301512569916379L;
+    @SuppressWarnings("unchecked")
+    public Object[] findFailedCountAndLastFailedDateForProcessId(BpelDAOConnection conn, String status, String processId) {
+        Object[] results = new Object[] {0, null};
+        
+        Query query = getSession().getNamedQuery(HProcessInstance.COUNT_FAILED_INSTANCES_BY_PROCESS_IDS_AND_STATES);
+        query.setParameterList("states", new InstanceFilter("status=" + status).convertFilterState());
+        query.setParameterList("processIds", new String[] {processId});
+        query.setResultTransformer(new ResultTransformer() {
+            private static final long serialVersionUID = 8034301512569916379L;
 
-			public List transformList(List collection) {
-				return collection;
-			}
-			public Object transformTuple(Object[] tuple, String[] aliases) {
-				return tuple;
-			}
-		});
-		
-		List result = query.list();
-		if( !result.isEmpty() ) {
-			results = (Object[])result.iterator().next();
-		}
-		
-		return results;
-	}
-	
-	public void prefetchActivityFailureCounts(Collection<ProcessInstanceDAO> instances) {
-		// do nothing; activity failures counts are already in a column 
-	}
+            public List transformList(List collection) {
+                return collection;
+            }
+            public Object transformTuple(Object[] tuple, String[] aliases) {
+                return tuple;
+            }
+        });
+        
+        List result = query.list();
+        if( !result.isEmpty() ) {
+            results = (Object[])result.iterator().next();
+        }
+        
+        return results;
+    }
+    
+    public void prefetchActivityFailureCounts(Collection<ProcessInstanceDAO> instances) {
+        // do nothing; activity failures counts are already in a column 
+    }
 }
