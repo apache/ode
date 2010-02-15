3a1354e43505f4beaa2eeadeed4932447e024ee2
Rafal Konrad Rusin <rr@apache.org>
2010-02-15 13:57:05 +0000

    ODE-751: FindBugs valueOf patch (By David Carver)
    git-svn-id: https://svn.apache.org/repos/asf/ode/trunk@910219 13f79535-47bb-0310-9956-ffa450edef68

--- utils/src/main/java/org/apache/ode/utils/CronExpression.java
+++ utils/src/main/java/org/apache/ode/utils/CronExpression.java
@@ -193,32 +193,32 @@ public class CronExpression implements Serializable, Cloneable {
     protected static final int YEAR = 6;
     protected static final int ALL_SPEC_INT = 99; // '*'
     protected static final int NO_SPEC_INT = 98; // '?'
-    protected static final Integer ALL_SPEC = new Integer(ALL_SPEC_INT);
-    protected static final Integer NO_SPEC = new Integer(NO_SPEC_INT);
+    protected static final Integer ALL_SPEC = Integer.valueOf(ALL_SPEC_INT);
+    protected static final Integer NO_SPEC = Integer.valueOf(NO_SPEC_INT);
     
     protected static final Map monthMap = new HashMap(20);
     protected static final Map dayMap = new HashMap(60);
     static {
-        monthMap.put("JAN", new Integer(0));
-        monthMap.put("FEB", new Integer(1));
-        monthMap.put("MAR", new Integer(2));
-        monthMap.put("APR", new Integer(3));
-        monthMap.put("MAY", new Integer(4));
-        monthMap.put("JUN", new Integer(5));
-        monthMap.put("JUL", new Integer(6));
-        monthMap.put("AUG", new Integer(7));
-        monthMap.put("SEP", new Integer(8));
-        monthMap.put("OCT", new Integer(9));
-        monthMap.put("NOV", new Integer(10));
-        monthMap.put("DEC", new Integer(11));
+        monthMap.put("JAN", Integer.valueOf(0));
+        monthMap.put("FEB", Integer.valueOf(1));
+        monthMap.put("MAR", Integer.valueOf(2));
+        monthMap.put("APR", Integer.valueOf(3));
+        monthMap.put("MAY", Integer.valueOf(4));
+        monthMap.put("JUN", Integer.valueOf(5));
+        monthMap.put("JUL", Integer.valueOf(6));
+        monthMap.put("AUG", Integer.valueOf(7));
+        monthMap.put("SEP", Integer.valueOf(8));
+        monthMap.put("OCT", Integer.valueOf(9));
+        monthMap.put("NOV", Integer.valueOf(10));
+        monthMap.put("DEC", Integer.valueOf(11));
 
-        dayMap.put("SUN", new Integer(1));
-        dayMap.put("MON", new Integer(2));
-        dayMap.put("TUE", new Integer(3));
-        dayMap.put("WED", new Integer(4));
-        dayMap.put("THU", new Integer(5));
-        dayMap.put("FRI", new Integer(6));
-        dayMap.put("SAT", new Integer(7));
+        dayMap.put("SUN", Integer.valueOf(1));
+        dayMap.put("MON", Integer.valueOf(2));
+        dayMap.put("TUE", Integer.valueOf(3));
+        dayMap.put("WED", Integer.valueOf(4));
+        dayMap.put("THU", Integer.valueOf(5));
+        dayMap.put("FRI", Integer.valueOf(6));
+        dayMap.put("SAT", Integer.valueOf(7));
     }
 
     private String cronExpression = null;
@@ -667,7 +667,7 @@ public class CronExpression implements Serializable, Cloneable {
                 throw new ParseException("'L' option is not valid here. (pos=" + i + ")", i);
             }
             TreeSet set = getSet(type);
-            set.add(new Integer(val));
+            set.add(Integer.valueOf(val));
             i++;
             return i;
         }
@@ -679,7 +679,7 @@ public class CronExpression implements Serializable, Cloneable {
                 throw new ParseException("'W' option is not valid here. (pos=" + i + ")", i);
             }
             TreeSet set = getSet(type);
-            set.add(new Integer(val));
+            set.add(Integer.valueOf(val));
             i++;
             return i;
         }
@@ -701,7 +701,7 @@ public class CronExpression implements Serializable, Cloneable {
             }
 
             TreeSet set = getSet(type);
-            set.add(new Integer(val));
+            set.add(Integer.valueOf(val));
             i++;
             return i;
         }
@@ -924,7 +924,7 @@ public class CronExpression implements Serializable, Cloneable {
 
         if ((incr == 0 || incr == -1) && val != ALL_SPEC_INT) {
             if (val != -1) {
-                set.add(new Integer(val));
+                set.add(Integer.valueOf(val));
             } else {
                 set.add(NO_SPEC);
             }
@@ -1005,7 +1005,7 @@ public class CronExpression implements Serializable, Cloneable {
         for (int i = startAt; i <= stopAt; i += incr) {
             if (max == -1) {
                 // ie: there's no max to overflow over
-                set.add(new Integer(i));
+                set.add(Integer.valueOf(i));
             } else {
                 // take the modulus to get the real value
                 int i2 = i % max;
@@ -1015,7 +1015,7 @@ public class CronExpression implements Serializable, Cloneable {
                     i2 = max;
                 }
 
-                set.add(new Integer(i2));
+                set.add(Integer.valueOf(i2));
             }
         }
     }
@@ -1457,7 +1457,7 @@ public class CronExpression implements Serializable, Cloneable {
             t = -1;
 
             // get year...................................................
-            st = years.tailSet(new Integer(year));
+            st = years.tailSet(Integer.valueOf(year));
             if (st != null && st.size() != 0) {
                 t = year;
                 year = ((Integer) st.first()).intValue();
