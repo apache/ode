7f68d134d3eea181edb860277e9eac947f2a9ffb
Tammo van Lessen <vanto@apache.org>
2011-01-07 00:44:25 +0000

    removing JDK6 @Overrides.
    git-svn-id: https://svn.apache.org/repos/asf/ode/branches/ode-1.3.5.x@1056143 13f79535-47bb-0310-9956-ffa450edef68

--- jbi-bundle/src/main/java/org/apache/ode/jbi/osgi/deployer/OdeExtenderImpl.java
+++ jbi-bundle/src/main/java/org/apache/ode/jbi/osgi/deployer/OdeExtenderImpl.java
@@ -53,243 +53,235 @@ public class OdeExtenderImpl implements OdeExtender, InitializingBean, Disposabl
     public OdeExtenderImpl() {
     }
 
-    @Override
     public void setBundleContext(BundleContext bundleContext) {
-	this.bundleContext = bundleContext;
+        this.bundleContext = bundleContext;
     }
 
-    @Override
     public void afterPropertiesSet() throws Exception {
-	// Create our executor.
-	executor = Executors.newSingleThreadExecutor(new OdeExtenderThreadFactory("ODE Extender"));
+        // Create our executor.
+        executor = Executors.newSingleThreadExecutor(new OdeExtenderThreadFactory("ODE Extender"));
 
-	// Start tracking bundles. We are looking for /deploy.xml as a signature
-	// to deploy the ODE BPEL bundle, if the bundle has at least one .bpel
-	// file.
-	int stateMask = Bundle.STARTING | Bundle.ACTIVE | Bundle.STOPPING | Bundle.INSTALLED | Bundle.UNINSTALLED;
-	this.tracker = new BundleTracker(this.bundleContext, stateMask, new ODEBundleTrackerCustomizer());
-	this.tracker.open();
+        // Start tracking bundles. We are looking for /deploy.xml as a signature
+        // to deploy the ODE BPEL bundle, if the bundle has at least one .bpel
+        // file.
+        int stateMask = Bundle.STARTING | Bundle.ACTIVE | Bundle.STOPPING | Bundle.INSTALLED | Bundle.UNINSTALLED;
+        this.tracker = new BundleTracker(this.bundleContext, stateMask, new ODEBundleTrackerCustomizer());
+        this.tracker.open();
     }
 
-    @Override
     public void destroy() throws Exception {
-	// Close the tracker.
-	BundleTracker tracker = this.tracker;
-	this.tracker = null;
-	if (tracker != null)
-	    tracker.close();
+        // Close the tracker.
+        BundleTracker tracker = this.tracker;
+        this.tracker = null;
+        if (tracker != null)
+            tracker.close();
 
-	// Drop our thread pool.
-	this.executor = null;
+        // Drop our thread pool.
+        this.executor = null;
     }
 
-    @Override
     public Bundle[] getBundles() {
-	return this.tracker.getBundles();
+        return this.tracker.getBundles();
     }
 
     public void setOdeComponent(Component odeComponent) {
-	this.odeComponent = odeComponent;
+        this.odeComponent = odeComponent;
     }
 
     public Component getOdeComponent() {
-	return odeComponent;
+        return odeComponent;
     }
 
     private boolean isBPELBundle(Bundle bundle) {
-	boolean Result = false;
+        boolean result = false;
 
-	// First see if there is a deploy.xml.
-	URL entry = bundle.getEntry("deploy.xml");
-	if (entry != null) {
-	    // Next, check if there's at least one BPEL file.
-	    @SuppressWarnings("rawtypes")
-	    Enumeration bpelFiles = bundle.findEntries("/", "*.bpel", false);
-	    if (bpelFiles != null && bpelFiles.hasMoreElements()) {
-		// Make sure there's a symbolic name.
-		if (bundle.getSymbolicName() != null) {
-		    // Make sure there's no bundle activator.
-		    // NOTE: if the "ServiceUnitActivator" is found, we hijack those bundles as well and manage them.
-		    Dictionary<?, ?> headers = bundle.getHeaders();
-		    Object activator = null;
-		    if (headers == null || (activator = headers.get(Constants.BUNDLE_ACTIVATOR)) == null || SU_ACTIVATOR_DEPRECATED.equals(activator)) {
-			if (LOG.isLoggable(Level.FINE))
-			    LOG.fine("Recognized ODE deployment bundle: " + bundle.getSymbolicName());
-			Result = true;
-		    } else
-			LOG.warning("Ignoring ODE bundle " + bundle.getSymbolicName() + " which has custom activator: " + activator);
-		} else
-		    LOG.warning("Ignoring ODE bundle " + bundle.getBundleId() + " which has no OSGi symbolic name");
-	    }
-	}
+        // First see if there is a deploy.xml.
+        URL entry = bundle.getEntry("deploy.xml");
+        if (entry != null) {
+            // Next, check if there's at least one BPEL file.
+            @SuppressWarnings("rawtypes")
+            Enumeration bpelFiles = bundle.findEntries("/", "*.bpel", false);
+            if (bpelFiles != null && bpelFiles.hasMoreElements()) {
+                // Make sure there's a symbolic name.
+                if (bundle.getSymbolicName() != null) {
+                    // Make sure there's no bundle activator.
+                    // NOTE: if the "ServiceUnitActivator" is found, we hijack those bundles as well and manage them.
+                    Dictionary<?, ?> headers = bundle.getHeaders();
+                    Object activator = null;
+                    if (headers == null || (activator = headers.get(Constants.BUNDLE_ACTIVATOR)) == null || SU_ACTIVATOR_DEPRECATED.equals(activator)) {
+                        if (LOG.isLoggable(Level.FINE))
+                            LOG.fine("Recognized ODE deployment bundle: " + bundle.getSymbolicName());
+                        result = true;
+                    } else
+                        LOG.warning("Ignoring ODE bundle " + bundle.getSymbolicName() + " which has custom activator: " + activator);
+                } else
+                    LOG.warning("Ignoring ODE bundle " + bundle.getBundleId() + " which has no OSGi symbolic name");
+            }
+        }
 
-	return Result;
+        return result;
     }
 
     private class ODEBundleTrackerCustomizer implements BundleTrackerCustomizer {
-	private Map<Long, OdeDeployedBundle> bundles = new ConcurrentHashMap<Long, OdeDeployedBundle>();
+        private Map<Long, OdeDeployedBundle> bundles = new ConcurrentHashMap<Long, OdeDeployedBundle>();
 
-	@Override
-	public void removedBundle(Bundle bundle, BundleEvent event, Object object) {
-	}
+        public void removedBundle(Bundle bundle, BundleEvent event, Object object) {
+        }
 
-	@Override
-	public void modifiedBundle(Bundle bundle, BundleEvent event, Object object) {
-	    if (event != null) {
-		if (LOG.isLoggable(Level.FINE))
-		    LOG.fine("Received " + getEventType(event.getType()) + " event for bundle: " + bundle.getSymbolicName());
-		switch (event.getType()) {
-		case BundleEvent.STARTED:
-		    executor.execute(new Start((OdeDeployedBundle) object));
-		    break;
-		case BundleEvent.STOPPING:
-		    executor.execute(new Stop((OdeDeployedBundle) object));
-		    break;
-		case BundleEvent.INSTALLED:
-		    executor.execute(new Install((OdeDeployedBundle) object));
-		    break;
-		case BundleEvent.UNINSTALLED:
-		    executor.execute(new Uninstall((OdeDeployedBundle) object));
-		    break;
-		case BundleEvent.UPDATED:
-		    executor.execute(new Update((OdeDeployedBundle) object));
-		    break;
-		}
+        public void modifiedBundle(Bundle bundle, BundleEvent event, Object object) {
+            if (event != null) {
+                if (LOG.isLoggable(Level.FINE))
+                    LOG.fine("Received " + getEventType(event.getType()) + " event for bundle: " + bundle.getSymbolicName());
+                switch (event.getType()) {
+                case BundleEvent.STARTED:
+                    executor.execute(new Start((OdeDeployedBundle) object));
+                    break;
+                case BundleEvent.STOPPING:
+                    executor.execute(new Stop((OdeDeployedBundle) object));
+                    break;
+                case BundleEvent.INSTALLED:
+                    executor.execute(new Install((OdeDeployedBundle) object));
+                    break;
+                case BundleEvent.UNINSTALLED:
+                    executor.execute(new Uninstall((OdeDeployedBundle) object));
+                    break;
+                case BundleEvent.UPDATED:
+                    executor.execute(new Update((OdeDeployedBundle) object));
+                    break;
+                }
 
-		// Do this outside the try/catch above. Last chance to drop a
-		// bundle.
-		if (event.getType() == BundleEvent.UNINSTALLED)
-		    bundles.remove(bundle.getBundleId());
-	    }
-	}
+                // Do this outside the try/catch above. Last chance to drop a
+                // bundle.
+                if (event.getType() == BundleEvent.UNINSTALLED)
+                    bundles.remove(bundle.getBundleId());
+            }
+        }
 
-	private String getEventType(int type) {
-	    switch (type) {
-	    case BundleEvent.INSTALLED:
-		return "installed";
-	    case BundleEvent.LAZY_ACTIVATION:
-		return "lazy activation";
-	    case BundleEvent.RESOLVED:
-		return "resolved";
-	    case BundleEvent.STARTED:
-		return "started";
-	    case BundleEvent.STARTING:
-		return "starting";
-	    case BundleEvent.STOPPED:
-		return "stopped";
-	    case BundleEvent.STOPPING:
-		return "stopping";
-	    case BundleEvent.UNINSTALLED:
-		return "uninstalled";
-	    case BundleEvent.UNRESOLVED:
-		return "unresolved";
-	    case BundleEvent.UPDATED:
-		return "updated";
-	    }
-	    return "(unknown: " + type + ")";
-	}
+        private String getEventType(int type) {
+            switch (type) {
+            case BundleEvent.INSTALLED:
+                return "installed";
+            case BundleEvent.LAZY_ACTIVATION:
+                return "lazy activation";
+            case BundleEvent.RESOLVED:
+                return "resolved";
+            case BundleEvent.STARTED:
+                return "started";
+            case BundleEvent.STARTING:
+                return "starting";
+            case BundleEvent.STOPPED:
+                return "stopped";
+            case BundleEvent.STOPPING:
+                return "stopping";
+            case BundleEvent.UNINSTALLED:
+                return "uninstalled";
+            case BundleEvent.UNRESOLVED:
+                return "unresolved";
+            case BundleEvent.UPDATED:
+                return "updated";
+            }
+            return "(unknown: " + type + ")";
+        }
 
-	@Override
-	public Object addingBundle(Bundle bundle, BundleEvent event) {
-	    Object Result = null;
+        public Object addingBundle(Bundle bundle, BundleEvent event) {
+            Object result = null;
 
-	    // Is this a BPEL bundle?
-	    if (isBPELBundle(bundle)) {
-		// Found BPEL signature; setup deployer.
-		OdeDeployedBundle deployer = bundles.get(bundle.getBundleId());
-		if (deployer == null)
-		    bundles.put(bundle.getBundleId(), deployer = new OdeDeployedBundle(bundle, OdeExtenderImpl.this));
-		Result = deployer;
+            // Is this a BPEL bundle?
+            if (isBPELBundle(bundle)) {
+                // Found BPEL signature; setup deployer.
+                OdeDeployedBundle deployer = bundles.get(bundle.getBundleId());
+                if (deployer == null)
+                    bundles.put(bundle.getBundleId(), deployer = new OdeDeployedBundle(bundle, OdeExtenderImpl.this));
+                result = deployer;
 
-		// Is this an initial bundle?
-		if (event == null) {
-		    // If the bundle is active, then we didn't start it. So
-		    // start it now.
-		    if (bundle.getState() == Bundle.ACTIVE)
-			executor.execute(new Start(deployer));
-		} else
-		    modifiedBundle(bundle, event, deployer);
-	    }
+                // Is this an initial bundle?
+                if (event == null) {
+                    // If the bundle is active, then we didn't start it. So
+                    // start it now.
+                    if (bundle.getState() == Bundle.ACTIVE)
+                        executor.execute(new Start(deployer));
+                } else
+                    modifiedBundle(bundle, event, deployer);
+            }
 
-	    return Result;
-	}
+            return result;
+        }
     }
 
     private static abstract class OperationClosure implements Runnable {
-	protected OdeDeployedBundle target;
+        protected OdeDeployedBundle target;
 
-	OperationClosure(OdeDeployedBundle target) {
-	    this.target = target;
-	}
+        OperationClosure(OdeDeployedBundle target) {
+            this.target = target;
+        }
 
-	@Override
-	public final void run() {
-	    String name = Thread.currentThread().getName();
-	    try {
-		Thread.currentThread().setName(name + ":" + getClass().getSimpleName() + ":" + target.getName());
-		perform();
-	    } catch (Exception e) {
-		LOG.log(Level.WARNING, "Could not perform '" + getClass().getSimpleName() + "' operation on bundle: " + target.getName(), e);
-	    } finally {
-		Thread.currentThread().setName(name);
-	    }
-	}
+        public final void run() {
+            String name = Thread.currentThread().getName();
+            try {
+                Thread.currentThread().setName(name + ":" + getClass().getSimpleName() + ":" + target.getName());
+                perform();
+            } catch (Exception e) {
+                LOG.log(Level.WARNING, "Could not perform '" + getClass().getSimpleName() + "' operation on bundle: " + target.getName(), e);
+            } finally {
+                Thread.currentThread().setName(name);
+            }
+        }
 
-	protected abstract void perform() throws Exception;
+        protected abstract void perform() throws Exception;
     }
 
     private static class Start extends OperationClosure {
-	Start(OdeDeployedBundle target) {
-	    super(target);
-	}
+        Start(OdeDeployedBundle target) {
+            super(target);
+        }
 
-	@Override
-	protected void perform() throws Exception {
-	    target.doStart();
-	}
+        @Override
+        protected void perform() throws Exception {
+            target.doStart();
+        }
     }
 
     private static class Stop extends OperationClosure {
-	Stop(OdeDeployedBundle target) {
-	    super(target);
-	}
+        Stop(OdeDeployedBundle target) {
+            super(target);
+        }
 
-	@Override
-	protected void perform() throws Exception {
-	    target.doStop();
-	}
+        @Override
+        protected void perform() throws Exception {
+            target.doStop();
+        }
     }
 
     private static class Install extends OperationClosure {
-	Install(OdeDeployedBundle target) {
-	    super(target);
-	}
+        Install(OdeDeployedBundle target) {
+            super(target);
+        }
 
-	@Override
-	protected void perform() throws Exception {
-	    target.doInstall();
-	}
+        @Override
+        protected void perform() throws Exception {
+            target.doInstall();
+        }
     }
 
     private static class Uninstall extends OperationClosure {
-	Uninstall(OdeDeployedBundle target) {
-	    super(target);
-	}
+        Uninstall(OdeDeployedBundle target) {
+            super(target);
+        }
 
-	@Override
-	protected void perform() throws Exception {
-	    target.doUninstall();
-	}
+        @Override
+        protected void perform() throws Exception {
+            target.doUninstall();
+        }
     }
 
     private static class Update extends OperationClosure {
-	Update(OdeDeployedBundle target) {
-	    super(target);
-	}
+        Update(OdeDeployedBundle target) {
+            super(target);
+        }
 
-	@Override
-	protected void perform() throws Exception {
-	    target.doUpdate();
-	}
+        @Override
+        protected void perform() throws Exception {
+            target.doUpdate();
+        }
     }
 }
