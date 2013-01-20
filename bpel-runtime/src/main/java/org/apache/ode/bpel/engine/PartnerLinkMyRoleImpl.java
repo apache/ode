4421f7612b0bc327cab7bdd1242371f2db838692
Hadrian Zbarcea <hadrian@apache.org>
2013-01-20 02:59:22 +0000

    ODE-989. Fix for intermittent IllegalArgumentExeption. Thanks Nandika for patch
    git-svn-id: https://svn.apache.org/repos/asf/ode/branches/ode-1.3.5.x@1435778 13f79535-47bb-0310-9956-ffa450edef68

--- bpel-runtime/src/main/java/org/apache/ode/bpel/engine/PartnerLinkMyRoleImpl.java
+++ bpel-runtime/src/main/java/org/apache/ode/bpel/engine/PartnerLinkMyRoleImpl.java
@@ -205,6 +205,7 @@ public class PartnerLinkMyRoleImpl extends PartnerLinkRoleImpl {
         if (mex.getDAO().getCreateTime() == null)
             mex.getDAO().setCreateTime(instance.getCurrentEventDateTime()); 
         
+        _process._engine.acquireInstanceLock(newInstance.getInstanceId());
         instance.execute();
     }
 
