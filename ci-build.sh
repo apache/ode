1137320e28e4a23aee63cab042cd946ba388af6a
sathwik <sathwik@apache.org>
2015-03-31 21:18:40 +0530

    Added the required buildr dependencies in Gemfile and updated the CI build script

--- ci-build.sh
+++ ci-build.sh
@@ -1,21 +1,36 @@
-#    Licensed to the Apache Software Foundation (ASF) under one or more
-#    contributor license agreements.  See the NOTICE file distributed with
-#    this work for additional information regarding copyright ownership.
-#    The ASF licenses this file to You under the Apache License, Version 2.0
-#    (the "License"); you may not use this file except in compliance with
-#    the License.  You may obtain a copy of the License at
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
 #
-#       http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-set -e
-export TEST_DUMMY_ENV_VAR=42;
-JAVA_OPTS="-XX:MaxPermSize=256m -Xmx2048m -Dlog4j.debug=true" TEST_PORTS="8888,7171" buildr _1.3.5_ clean test
+#!/bin/sh
 
-echo SUCCESS
+docker pull vanto/apache-buildr:latest-jruby-jdk7
 
+export JAVA_OPTS="-Xmx1024M -XX:MaxPermSize=512M"
+BUILDR_ARGS="$@"
+CONTAINER_USERNAME="dummy"
+CONTAINER_GROUPNAME="dummy"
+HOMEDIR="/home/$CONTAINER_USERNAME"
+GROUP_ID=$(id -g)
+USER_ID=$( id -u)
+
+CREATE_USER_COMMAND="groupadd -f -g $GROUP_ID $CONTAINER_GROUPNAME && useradd -u $USER_ID -g $CONTAINER_GROUPNAME $CONTAINER_USERNAME && mkdir --parent $HOMEDIR && chown -R $CONTAINER_USERNAME:$CONTAINER_GROUPNAME $HOMEDIR"
+
+BUNDLER_COMMAND="/opt/jruby/bin/jruby -S bundler install --gemfile=/workspace/Gemfile"
+
+BUILDR_COMMAND="su $CONTAINER_USERNAME -c '/opt/jruby/bin/jruby -S buildr $BUILDR_ARGS'"
+
+FINAL_COMMAND="$CREATE_USER_COMMAND && $BUNDLER_COMMAND && $BUILDR_COMMAND"
+
+docker run --rm -t -i -e JAVADOC=no -e JAVA_OPTS -v `pwd`:/workspace -v $HOME/.m2:/home/dummy/.m2  -v /tmp:/tmp --entrypoint bash vanto/apache-buildr:latest-jruby-jdk7 -c "$FINAL_COMMAND"
