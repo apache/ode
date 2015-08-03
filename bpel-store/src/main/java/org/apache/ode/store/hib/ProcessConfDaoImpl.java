122d17904396d9f5f30ad0dad7de1d33d57c9c1b
sathwik <sathwik@apache.org>
2015-08-03 15:21:41 +0530

    ODE-1035: Added LOB column

--- bpel-store/src/main/java/org/apache/ode/store/hib/ProcessConfDaoImpl.java
+++ bpel-store/src/main/java/org/apache/ode/store/hib/ProcessConfDaoImpl.java
@@ -68,7 +68,7 @@ public class ProcessConfDaoImpl extends HibObj implements ProcessConfDAO {
      * @hibernate.map table="STORE_PROCESS_PROP" role="properties_"
      * @hibernate.collection-key column="propId" foreign-key="none"
      * @hibernate.collection-index column="name" type="string" 
-     * @hibernate.collection-element column="value" type="string" length="2048"
+     * @hibernate.collection-element column="data" type="text"
      */
     public Map<String,String> getProperties_() {
         return _properties;
