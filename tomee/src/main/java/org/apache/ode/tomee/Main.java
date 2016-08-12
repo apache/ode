30d959c8c1e689026142adba32f40881db6b61fd
sathwik <sathwik@apache.org>
2016-08-12 20:38:04 +0530

    ODE-1060: Check for home system property

--- tomee/src/main/java/org/apache/ode/tomee/Main.java
+++ tomee/src/main/java/org/apache/ode/tomee/Main.java
@@ -24,25 +24,30 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         String home = System.getProperty("ode.server.home");
-        final TomeeLauncher launcher = new TomeeLauncher(home);
-        try {
-            launcher.initialize();
-            launcher.start();
 
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                public void run() {
-                    try {
-                        launcher.stop();
-                        System.out.println("Continer stopped");
-                    } catch (final Exception e) {
-                        e.printStackTrace();
+        if(home != null) {
+            final TomeeLauncher launcher = new TomeeLauncher(home);
+            try {
+                launcher.initialize();
+                launcher.start();
+
+                Runtime.getRuntime().addShutdownHook(new Thread() {
+                    public void run() {
+                        try {
+                            launcher.stop();
+                            System.out.println("Continer stopped");
+                        } catch (final Exception e) {
+                            e.printStackTrace();
+                        }
                     }
-                }
-            });
+                });
 
-            launcher.deploy(new File(home,"webapps/ode"), "ode");
-        } catch (Exception e) {
-            e.printStackTrace();
+                launcher.deploy(new File(home,"webapps/ode"), "ode");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Cannot start the container. Kindly set system property ode.server.home");
         }
     }
 }
