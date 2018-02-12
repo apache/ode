2f55f35d069130c374baf1ac6b64ec843e4e4c8c
sathwik <sathwik@apache.org>
2018-02-12 12:27:42 +0530

    added release script

--- ode-release.sh
+++ ode-release.sh
@@ -36,6 +36,19 @@ BUILDR_COMMAND="su $CONTAINER_USERNAME -c 'buildr $BUILDR_ARGS'"
 
 FINAL_COMMAND="$CREATE_USER_COMMAND && $BUNDLER_COMMAND && $BUILDR_COMMAND"
 
+
+## For release set these arguments with proper values
+##  export JAVADOC=on
+##  export BUILDR_ENV=production
+##  (Append -SNAPSHOT for every next version)
+##  export NEXT_VERSION=1.3.8-SNAPSHOT
+##  export GNUPGHOME="$HOME/.gnupg"
+##  export GPG_USER=
+##  export GPG_PASS=
+
+##  mount volume for release
+##  -v $GNUPGHOME:/home/dummy/.gnupg
+
 docker run --rm \
     -e JAVADOC=$JAVADOC \
     -e NEXT_VERSION=$NEXT_VERSION \
@@ -47,4 +60,5 @@ docker run --rm \
     -v $HOME/.m2:/home/dummy/.m2 \
     -v $HOME/.buildr:/home/dummy/.buildr \
     -v /tmp:/tmp \
+    -v $GNUPGHOME:/home/dummy/.gnupg \
     --entrypoint bash sathwik/apache-buildr:latest-jruby-jdk8 -c "$FINAL_COMMAND";
